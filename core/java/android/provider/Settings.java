719122714a20f583f8c0df2bc0a786f8ddb34d10
Paul Mongold <pmongold@gmail.com>
2012-08-08 01:10:28 -0400

    6-bar signal icon sytle [1/2]
    Change-Id: I13f53c9500b44286c1cb93fd27ad1275b1d29484

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3133,6 +3133,12 @@ public final class Settings {
         public static final String SYSTEMUI_WEATHER_NOTIFICATION = "cfx_weather_notification";
 
         /**
+         * Toggle for 6 bar signal bars
+         * @hide
+         */
+        public static final String STATUSBAR_6BAR_SIGNAL = "statusbar_6bar_signal";
+
+        /**
          * HALO enabled, should default to 0 (no, HALO is disabled)
          * @hide
          */
