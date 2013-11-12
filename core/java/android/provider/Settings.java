fcfe9072ac7dca2762f5cb4e5a032e8c607c134c
Lars Greiss <kufikugel@googlemail.com>
2013-11-12 01:30:28 +0100

    Add back increasing ring feature.
    Change-Id: Id950d3c19a05e4efb9ed0b36be6a7482dd173965

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2129,6 +2129,24 @@ public final class Settings {
         public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Minimum volume index for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_MIN_VOLUME = "increasing_ring_min_vol";
+
+        /**
+         * Time (in ms) between ringtone volume increases
+         * @hide
+         */
+        public static final String INCREASING_RING_INTERVAL = "increasing_ring_interval";
+
+        /**
          * Boolean value whether to link ringtone and notification volumes
          *
          * @hide
