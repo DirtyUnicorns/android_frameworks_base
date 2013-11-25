22b8d0bc70a89c6c46930b72fbefd37324b5d5bf
xplodwild <xplod@ouverta.fr>
2013-11-25 22:54:39 +0100

    Add time-context headers to the notification header [1/2]
    This commit adds image headers to the notification panel header,
    based on the date/time.
    
    There is a default provider that provides sky pictures
    as well as special events like Christmas and new
    year's eve.
    
    More will be coming, with eventually a way to use an external
    (as in, Internet) provider for those pictures.
    
    Change-Id: Ie3321e9a3e0637d6ba8094dd46aee36c41354713

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -151,6 +151,7 @@ import com.android.systemui.R;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
+import com.android.systemui.omni.StatusBarHeaderMachine;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recent.ScreenPinningRequest;
 import com.android.systemui.statusbar.ActivatableNotificationView;
@@ -707,6 +708,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mAutohideSuspended;
     private int mStatusBarMode;
     private int mNavigationBarMode;
+    private StatusBarHeaderMachine mStatusBarHeaderMachine;
 
     private ViewMediatorCallback mKeyguardViewMediatorCallback;
     private ScrimController mScrimController;
@@ -1291,6 +1293,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         startGlyphRasterizeHack();
         setBottomIconsColors();
+
+        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
+        mStatusBarHeaderMachine.addObserver(mHeader);
+        mStatusBarHeaderMachine.updateEnablement();
+
         return mStatusBarView;
     }
 
