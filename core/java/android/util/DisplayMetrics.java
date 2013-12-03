7d5f92e4da10633b154cc068f2a4ba0df88293b2
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-12-02 18:04:25 -0600

    Support for fake density
    Change-Id: Ia2c7a7ba3af4242a2abbe503becae5519ff1f7e5

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -305,6 +305,9 @@ public class DisplayMetrics {
     }
 
     private static int getDeviceDensity() {
+        // Check for faked density
+        int density = SystemProperties.getInt("persist.sys.fake_density", 0);
+        if (density > 0) return density;
         // qemu.sf.lcd_density can be used to override ro.sf.lcd_density
         // when running in the emulator, allowing for dynamic configurations.
         // The reason for this is that ro.sf.lcd_density is write-once and is
