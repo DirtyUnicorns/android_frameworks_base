380745bda11170142de9a075afe2c76c39f22a0d
Alex <mazdarider23@gmail.com>
2013-12-13 20:19:04 -0600

    Revert "[1/2] Frameworks: Optional statusbar battery icons"
    This reverts commit 9a8862df28214a165b6587970158d6328461221f.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -680,9 +680,6 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         mBatteryCallback.refreshView(mBatteryTile, mBatteryState);
     }
     void refreshBatteryTile() {
-        if (mBatteryCallback == null) {
-            return;
-        }
         mBatteryCallback.refreshView(mBatteryTile, mBatteryState);
     }
 
