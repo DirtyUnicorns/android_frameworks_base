fb1cbaa6b37a010da894bf518e1546032f99bada
Alex <mazdarider23@gmail.com>
2013-12-13 20:34:23 -0600

    Revert "[1/3] Base: Squashed commits for 4.4 active display"
    This reverts commit b7b15557fb4a4ef944701169c0f306148fd13390.

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -32,15 +32,10 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.graphics.Paint;
-import android.graphics.Paint.Align;
-import android.graphics.Path;
-import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
-import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -52,7 +47,6 @@ import android.view.accessibility.AccessibilityManager;
 
 import com.android.internal.R;
 
-import java.lang.Math;
 import java.util.ArrayList;
 
 /**
@@ -100,8 +94,6 @@ public class GlowPadView extends View {
     private static final float TARGET_SCALE_COLLAPSED = 0.8f;
     private static final float RING_SCALE_EXPANDED = 1.0f;
     private static final float RING_SCALE_COLLAPSED = 0.5f;
-    private static final float HANDLE_TEXT_RADIUS = 120f;
-    private static final float MAX_TEXT_ARC_RADIANS = (float) (270.0 / 170.0 * Math.PI);
 
     private ArrayList<TargetDrawable> mTargetDrawables = new ArrayList<TargetDrawable>();
     private AnimationBundle mWaveAnimations = new AnimationBundle();
@@ -125,12 +117,6 @@ public class GlowPadView extends View {
     private int mMaxTargetWidth;
     private float mRingScaleFactor = 1f;
     private boolean mAllowScaling;
-    private boolean mDrawOuterRing = true;
-
-    private Paint mPaintText;
-    private String mHandleText = "";
-    private int mTextRadius;
-    private float mMaxTextArcLength;
 
     private float mOuterRadius = 0.0f;
     private float mSnapMargin = 0.0f;
@@ -142,7 +128,6 @@ public class GlowPadView extends View {
     private Paint mArcPaint;
     private RectF mArcRect;
     private float mArcAngle = 0f;
-    private ArrayList<TargetDrawable> mNewTargetDrawables;
 
     private class AnimationBundle extends ArrayList<Tweener> {
         private static final long serialVersionUID = 0xA84D78726F127468L;
@@ -305,16 +290,6 @@ public class GlowPadView extends View {
                                  mHandleDrawable.getPositionY() - mHandleDrawable.getHeight()/2,
                                  mHandleDrawable.getPositionX() + mHandleDrawable.getWidth()/2,
                                  mHandleDrawable.getPositionY() + mHandleDrawable.getHeight()/2);
-
-        mPaintText = new Paint();
-        mPaintText.setAntiAlias(true);
-        mPaintText.setColor(res.getColor(android.R.color.white));
-        mPaintText.setTextAlign(Align.CENTER);
-        mPaintText.setStyle(Paint.Style.FILL_AND_STROKE);
-        mPaintText.setFakeBoldText(true);
-        mPaintText.setTextSize(res.getDimensionPixelSize(R.dimen.glowpad_notification_font_size));
-        mTextRadius = res.getDimensionPixelSize(R.dimen.glowpad_notification_text_radius);
-        mMaxTextArcLength = mTextRadius * MAX_TEXT_ARC_RADIANS;
     }
 
     private int getResourceId(TypedArray a, int id) {
@@ -403,7 +378,6 @@ public class GlowPadView extends View {
                 startBackgroundAnimation(0, 0.0f);
                 mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
                 mHandleDrawable.setAlpha(1.0f);
-                mPaintText.setAlpha(255);
                 break;
 
             case STATE_START:
@@ -411,7 +385,6 @@ public class GlowPadView extends View {
                 break;
 
             case STATE_FIRST_TOUCH:
-                mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
                 deactivateTargets();
                 showTargets(true);
@@ -423,14 +396,12 @@ public class GlowPadView extends View {
                 break;
 
             case STATE_TRACKING:
-                mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 1.0f, null);
                 break;
 
             case STATE_SNAP:
                 // TODO: Add transition states (see list_selector_background_transition.xml)
-                mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 0.0f, null);
                 break;
@@ -648,14 +619,6 @@ public class GlowPadView extends View {
         }
     }
 
-    private void internalSetTargetResources(ArrayList<TargetDrawable> drawList) {
-        mTargetResourceId = 0;
-        mTargetDrawables = drawList;
-        updateTargetPositions(mWaveCenterX, mWaveCenterY);
-        updatePointCloudPosition(mWaveCenterX, mWaveCenterY);
-        hideTargets(false, false);
-    }
-
     /**
      * Loads an array of drawables from the given resourceId.
      *
@@ -672,15 +635,6 @@ public class GlowPadView extends View {
 
     public int getTargetResourceId() {
         return mTargetResourceId;
-    }
-
-    public void setTargetResources(ArrayList<TargetDrawable> drawList) {
-        if (mAnimatingTargets) {
-            // postpone this change until we return to the initial state
-            mNewTargetDrawables = drawList;
-        } else {
-            internalSetTargetResources(drawList);
-        }
     }
 
     /**
@@ -1254,7 +1208,7 @@ public class GlowPadView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         mPointCloud.draw(canvas);
-        if (mDrawOuterRing) mOuterRing.draw(canvas);
+        mOuterRing.draw(canvas);
         final int ntargets = mTargetDrawables.size();
         for (int i = 0; i < ntargets; i++) {
             TargetDrawable target = mTargetDrawables.get(i);
@@ -1272,32 +1226,6 @@ public class GlowPadView extends View {
 
             canvas.drawArc(mArcRect, -90, mArcAngle, false, mArcPaint);
         }
-        if (!TextUtils.isEmpty(mHandleText) && mPaintText.getAlpha() != 0) {
-            float x = mHandleDrawable.getPositionX();
-            float y = mHandleDrawable.getPositionY();
-            Path circle = new Path();
-            circle.addCircle(x, y, mTextRadius, Path.Direction.CW);
-            canvas.rotate(90f, x, y);
-            canvas.drawTextOnPath(mHandleText, circle, 0, 0, mPaintText);
-        }
-    }
-
-    public void setHandleDrawable(TargetDrawable d) {
-        float x = mHandleDrawable.getPositionX();
-        float y = mHandleDrawable.getPositionY();
-        mHandleDrawable = new TargetDrawable(d);
-        mHandleDrawable.setPositionX(x);
-        mHandleDrawable.setPositionY(y);
-    }
-
-    public void setHandleText(String text) {
-        mHandleText = TextUtils.ellipsize(
-                text, new TextPaint(mPaintText), mMaxTextArcLength,
-                TextUtils.TruncateAt.END).toString();
-    }
-
-    public void setDrawOuterRing(boolean drawOuterRing) {
-        mDrawOuterRing = drawOuterRing;
     }
 
     public void setOnTriggerListener(OnTriggerListener listener) {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -87,8 +87,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
@@ -277,8 +275,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
-
-    private boolean mRecreating = false;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -517,11 +513,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
-        }
-
-        if (mRecreating) {
-        } else {
-            addActiveDisplayView();
         }
 
         // figure out which pixel-format to use for the status bar.
@@ -2187,7 +2178,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHandler.postDelayed(mAutohide, 350); // longer than app gesture -> flag clear
     }
 
-    public boolean areLightsOn() {
+    private boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2207,7 +2198,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     }
 
-    @Override
     public void topAppWindowChanged(boolean showMenu) {
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
