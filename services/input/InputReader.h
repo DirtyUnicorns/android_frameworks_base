3397f688a677ad337d5b4ecb30f571bd07c10ff0
Alex <mazdarider23@gmail.com>
2013-12-13 20:31:19 -0600

    Revert "Disable touch while using the stylus (Galaxy Note)"
    This reverts commit a19f78cec0cb12a0fd00ece274e63a4b2658068a.

--- services/input/InputReader.h
+++ services/input/InputReader.h
@@ -224,9 +224,6 @@ struct InputReaderConfiguration {
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
 
-    // Ignore finger touches this long after the stylus has been used (including hover)
-    nsecs_t stylusPalmRejectionTime;
-
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
             pointerVelocityControlParameters(1.0f, 500.0f, 3000.0f, 3.0f),
@@ -243,9 +240,7 @@ struct InputReaderConfiguration {
             pointerGestureSwipeMaxWidthRatio(0.25f),
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
-            showTouches(false),
-            stylusPalmRejectionTime(50 * 10000000LL) // 50 ms
-    { }
+            showTouches(false) { }
 
     bool getDisplayInfo(bool external, DisplayViewport* outViewport) const;
     void setDisplayInfo(bool external, const DisplayViewport& viewport);
@@ -1635,9 +1630,6 @@ private:
     VelocityControl mWheelXVelocityControl;
     VelocityControl mWheelYVelocityControl;
 
-    // The time the stylus event was processed by any TouchInputMapper
-    static nsecs_t mLastStylusTime;
-
     void sync(nsecs_t when);
 
     bool consumeRawTouches(nsecs_t when, uint32_t policyFlags);
@@ -1690,8 +1682,6 @@ private:
     const VirtualKey* findVirtualKeyHit(int32_t x, int32_t y);
 
     void assignPointerIds();
-
-    bool rejectPalm(nsecs_t when);
 };
 
 
