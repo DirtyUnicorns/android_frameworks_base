2fed6528ce1a103295044d96e8782f58a38ba894
0xD34D <clark@scheffsblend.com>
2013-12-26 16:59:14 -0600

    Gesture Anywhere
    When enabled a user can activate the gesture anywhere view by touching
    the designated trigger area.  If a gesture is recognized as one the user
    has created and assigned to an action, that action will be performed.
    Since this is part of the system UI and implemented as an overlay, it
    will be available and usable system wide, with the exception of the
    keyguard for obvious reasons.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -29,6 +29,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -632,6 +634,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -722,6 +725,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
 
             addActiveDisplayView();
+            /* ChaosLab: GestureAnywhere - BEGIN */
+            addGestureAnywhereView();
+            /* ChaosLab: GestureAnywhere - END */
         }
 
         addSidebarView();
