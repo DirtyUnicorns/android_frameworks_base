936435f7c79480e3087e200112f7ca4096fc064d
garwedgess <garethwilliams21@gmail.com>
2014-01-18 21:20:34 +0530

    Breathing missedcall/sms/voicemail [1/4]
    Change-Id: I1b7f40c2fc5c6612303cf6575e3efe8e992cecae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2636,6 +2636,24 @@ public final class Settings {
         public static final String STATUS_BAR_TRAFFIC_ENABLE = "status_bar_traffic_enable";
 
         /**
+         * Give SMS notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+         * Give Missed call notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+        /**
+         * Give Voicemail notifications a breathing effect
+         * @hide
+         */
+        public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
+
+        /**
          * Show Screenshot in Power Menu
          * @hide
          */
