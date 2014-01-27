473b4f2891bbc5e7f6c08416dbc2d848d927a5e3
0xD34D <clark@scheffsblend.com>
2014-01-27 00:09:43 -0600

    Gesture Lockscreen [1/2]
    This adds the ability to us a hand drawn gesture as
    a means to unlock the device.
    
    In the case that the gesture is forgotton or not
    being recognized correctly, the fallback will be the
    same as when using patterns
    
    Androidx:
    Some changes needed for 5.0
    
    pimpmaneaton:
    Some changes needed for 5.1
    
    Change-Id: Iedaed283bfc317d4b14f21bad6fca1a3ffd4a190

--- packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
@@ -37,7 +37,8 @@ public class KeyguardSecurityModel {
         Biometric, // Unlock with a biometric key (e.g. finger print or face unlock)
         Account, // Unlock by entering an account's login and password.
         SimPin, // Unlock by entering a sim pin.
-        SimPuk // Unlock by entering a sim puk
+        SimPuk, // Unlock by entering a sim puk
+        Gesture // unlock by drawing a gesture
     }
 
     private Context mContext;
@@ -114,6 +115,12 @@ public class KeyguardSecurityModel {
                             SecurityMode.Account : SecurityMode.Pattern;
                     }
                     break;
+                case DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK:
+                    if (mLockPatternUtils.isLockGestureEnabled()) {
+                        mode = mLockPatternUtils.isPermanentlyLocked() ?
+                            SecurityMode.Account : SecurityMode.Gesture;
+                    }
+                    break;
 
                 default:
                     throw new IllegalStateException("Unknown security quality:" + security);
@@ -134,7 +141,8 @@ public class KeyguardSecurityModel {
         if (isBiometricUnlockEnabled() && !isBiometricUnlockSuppressed()
                 && (mode == SecurityMode.Password
                         || mode == SecurityMode.PIN
-                        || mode == SecurityMode.Pattern)) {
+                        || mode == SecurityMode.Pattern
+                        || mode == SecurityMode.Gesture )) {
             return SecurityMode.Biometric;
         }
         return mode; // no alternate, return what was given
@@ -151,6 +159,8 @@ public class KeyguardSecurityModel {
             case Biometric:
                 return getSecurityMode();
             case Pattern:
+                return SecurityMode.Account;
+            case Gesture:
                 return SecurityMode.Account;
         }
         return mode; // no backup, return current security mode
