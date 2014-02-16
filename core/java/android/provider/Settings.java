ee97b010b47e5fc903bbc3eab8c33b5962d7b2fd
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-02-15 21:58:17 -0500

    Frameworks: Open source google dialer and more 4/5
    Change-Id: I6cd8b8ac80fe39bde13fa92d0a3ab90c33ab9e71

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3530,6 +3530,34 @@ public final class Settings {
         public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
 
         /**
+         * Enable looking up of phone numbers of nearby places
+         *
+         * @hide
+         */
+        public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
+
+        /**
+         * Enable looking up of information of phone numbers not in the contacts
+         *
+         * @hide
+         */
+        public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
+
+        /**
+         * The forward lookup provider
+         *
+         * @hide
+         */
+        public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+
+        /**
+         * The reverse lookup provider
+         *
+         * @hide
+         */
+        public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
+
+        /**
          * QuickSettings configuration
          * @hide
          */
