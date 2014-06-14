6f8d3b1bfe6ee5cce613484633664fc981887933
Alex <mazdarider23@gmail.com>
2014-06-14 05:52:39 -0400

    Ad blocker toggle [1/2]
    Halo Toggle [1/2]
    
    Change-Id: I5e3b3b7c656ebbec7436afc3b8cd8e150f99952b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -53,6 +53,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.slim.QSConstants.TILE_GPS;
 import static com.android.internal.util.slim.QSConstants.TILE_FCHARGE;
+import static com.android.internal.util.slim.QSConstants.TILE_ADBLOCKER;
 import static com.android.internal.util.slim.QSConstants.TILE_HALO;
 import static com.android.internal.util.slim.QSConstants.TILE_SCREENSHOT;
 import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
@@ -108,6 +109,7 @@ import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.GpsTile;
 import com.android.systemui.quicksettings.FastChargeTile;
+import com.android.systemui.quicksettings.AdblockerTile;
 import com.android.systemui.quicksettings.HaloTile;
 import com.android.systemui.quicksettings.ScreenshotTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
@@ -258,6 +260,8 @@ public class QuickSettingsController {
                 qs = new GpsTile(mContext, this);
             } else if (tile.contains(TILE_FCHARGE)) {
                 qs = new FastChargeTile(mContext, this);
+            } else if (tile.contains(TILE_ADBLOCKER)) {
+                qs = new AdblockerTile(mContext, this);
             } else if (tile.equals(TILE_HALO)) {
                 qs = new HaloTile(mContext, this);
             } else if (tile.equals(TILE_SCREENSHOT)) {
