71e36e29054ea2838d1a869b3a150d7a3ecea68d
Raj Yengisetty <rajesh@cyngn.com>
2014-11-18 11:38:59 -0800

    Fix - place Clear Recents button above the nav bar.
    Change-Id: Ie7059cd1f71bae28ff25f367221b7c7ba0131932

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -39,6 +39,7 @@ import android.widget.FrameLayout;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.systemui.R;
+import android.widget.LinearLayout;
 import com.android.systemui.recents.Constants;
 import com.android.systemui.recents.RecentsAppWidgetHostView;
 import com.android.systemui.recents.RecentsConfiguration;
