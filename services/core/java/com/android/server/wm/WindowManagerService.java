e124a7f97a19d9d9737c9cc20f6baae559f9526f
Daniel 2 Olofsson <daniel2.olofsson@sonymobile.com>
2014-11-24 13:15:18 +0100

    Adding thread safety to disposing DragInputEventReceiver
    Fix is preventing that native representation of InputEventReceiver is
    not removed or deleted during use, through ongoing call to
    InputEventReceiver.finishInputEvent, while being disposed.
    This is done by assuring that same thread calling finishInputEvent also
    handles the actual disposal of InputEventReceiver.
    
    Note that fix handles new input events that may come in due to delayed
    disposal as non-drag events and will finish these upon arrival until
    proper thread has removed native receiver, input channels are closed or
    new InputEventReceiver is registered.
    
    Without fix a SEGV_MAPERR native crash may occur due to native object
    NativeInputEventReceiver being released from another thread while being
    accessed in finishInputEvent call on proper looper thread.
    
    Change-Id: I654be012f1ab9a366c2fb4e75587e7a9302a0169

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -708,9 +708,24 @@ public class WindowManagerService extends IWindowManager.Stub
 
     private WindowContentFrameStats mTempWindowRenderStats;
 
+    private boolean mDragStateDisposed;
+
     final class DragInputEventReceiver extends InputEventReceiver {
         public DragInputEventReceiver(InputChannel inputChannel, Looper looper) {
             super(inputChannel, looper);
+        }
+
+        public void requestDispose() {
+            mDragStateDisposed = true;
+            if (mH.getLooper().isCurrentThread()) {
+                dispose();
+            } else {
+                mH.post(new Runnable() {
+                    public void run() {
+                        dispose();
+                    }
+                });
+            }
         }
 
         @Override
@@ -719,6 +734,7 @@ public class WindowManagerService extends IWindowManager.Stub
             try {
                 if (event instanceof MotionEvent
                         && (event.getSource() & InputDevice.SOURCE_CLASS_POINTER) != 0
+                        && !mDragStateDisposed
                         && mDragState != null) {
                     final MotionEvent motionEvent = (MotionEvent)event;
                     boolean endDrag = false;
