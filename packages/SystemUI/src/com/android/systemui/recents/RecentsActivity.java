0485b6f59fdc0b14afc53f1c4ac8e5d848226987
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-25 21:42:53 +0200

    Fix Unused Space at Recents When Search Bar is Disabled
    Change-Id: If0d122c015a9e507ab689968cda15e9c15c23657

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
@@ -50,6 +51,7 @@ import com.android.systemui.recents.views.DebugOverlayView;
 import com.android.systemui.recents.views.RecentsView;
 import com.android.systemui.recents.views.SystemBarScrimViews;
 import com.android.systemui.recents.views.ViewAnimation;
+import com.android.systemui.recents.RecentsConfiguration;
 import android.provider.Settings;
 
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
@@ -267,6 +269,20 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                }
                 }
         }
+
+	// Update search bar space height
+        Resources reso = getResources();
+
+        if (Settings.System.getInt(getContentResolver(),
+                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) != 0) {
+        RecentsConfiguration.searchBarSpaceHeightPx = 0;
+
+		}
+
+        if (Settings.System.getInt(getContentResolver(),
+                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) != 1) {
+	RecentsConfiguration.searchBarSpaceHeightPx = reso.getDimensionPixelSize(R.dimen.recents_search_bar_space_height);
+		}
 
         // Animate the SystemUI scrims into view
         mScrimViews.prepareEnterRecentsAnimation();
