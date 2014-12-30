1d35f8a791059d239f63e9f0d459fc9bc7121fbd
Ravinder Konka <rkonka@codeaurora.org>
2014-12-30 16:57:36 +0530

    Tethering: Set sys.usb.tethering to true on Tethering
    Set the system Property "sys.usb.tethering" to true when
    tethering is enabled on UI
    
    Change-Id: I805a38b2d3ed055a562298d71f33fd5030b3a4a0

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -46,6 +46,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
@@ -810,6 +811,14 @@ public class Tethering extends BaseNetworkObserver {
             Log.e(TAG, "Error listing Interfaces", e);
             return;
         }
+
+        final String usbSysctlKey = "sys.usb.tethering";
+        SystemProperties.set(usbSysctlKey, "false");
+
+        if (enable) {
+           SystemProperties.set(usbSysctlKey, "true");
+        }
+
         for (String iface : ifaces) {
             if (isUsb(iface)) {
                 int result = (enable ? tether(iface) : untether(iface));
