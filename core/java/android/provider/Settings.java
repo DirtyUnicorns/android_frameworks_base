3f239f9dfcd894244664ae881eedbe9e565f6182
Lars Greiss <kufikugel@googlemail.com>
2015-01-04 01:09:33 -0500

    Less notifications sound [1/2]
    Change-Id: I4e7c3d6cf797766b18d057ed322aee45d1edafc7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3067,6 +3067,12 @@ public final class Settings {
         public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
         /**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
+
+        /**
          * Volume key controls ringtone or media sound stream
          *
          * @hide
