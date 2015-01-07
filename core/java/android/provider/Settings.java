4cce72f672489b677d3d5f28f02195b8ceea4740
Alex Cruz <mazdarider23@gmail.com>
2015-01-07 18:57:20 -0500

    Pimp the power menu out [1/2]
    - Add screenshot toggle (credit/thanks David van Tonder)
    - Made powermenu toggleable (power, reboot, screenshot, settings, soundpanel and lockdown)
    - Added new icons for screenshot and power/reboot
    
    Change-Id: I538ab88ce18a4e1da3a42786dea6fd010b9a933d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3534,6 +3534,55 @@ public final class Settings {
          */
 
         /**
+         * Whether to display sound panel in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SOUNDPANEL = "powermenu_soundpanel";
+
+        /**
+         * Whether to display screenshot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENSHOT = "powermenu_screenshot";
+
+        /**
+         * Whether to display settings in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SETTINGS = "powermenu_settings";
+
+        /**
+         * Whether to display lock in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_LOCKDOWN = "powermenu_lockdown";
+
+        /**
+         * Whether to display airplane in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_AIRPLANE = "powermenu_airplane";
+
+        /**
+         * Whether to display reboot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_REBOOT = "powermenu_reboot";
+
+        /**
+         * Whether to display power in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_POWER = "powermenu_power";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
