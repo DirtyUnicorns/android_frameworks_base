0971be860f52a29bc1268e1ee10abfc14f2980c4
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-21 03:53:31 +0000

    Reset timeout when expanding volume panel
    Change-Id: Iad3ba9367d12c60807347baf3e7c06a9d0b7d49a

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -750,6 +750,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 @Override
                 public void onClick(View v) {
                     expandVolumePanel();
+                    resetTimeout();
                 }
             });
             sc.seekbarView.setMax(getStreamMaxVolume(streamType));
