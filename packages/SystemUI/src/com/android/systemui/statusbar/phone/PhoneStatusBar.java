1f5ad932b1921e449587d2ef2c9a75d71fba101f
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-01-28 22:30:24 +0100

    Lock screen: Weather panel fwbase part 1.
    Based on CyanogenMod:
    https://github.com/CyanogenMod/android_frameworks_base/commit/d855e463bb934f41cee72a893609626de0a03f25
    https://github.com/CyanogenMod/android_frameworks_base/commit/1511e96f191bc4c9f191e417f2c0ee69058f863a
    
    Additional changes:
    
    - Move the weather panel to the bottom of the clock
    - Option to show/hide the weather panel
    - Option to show/hide the location
    
    Change-Id: Ie9a955204b1f90bdddd5cf9133cbc13cd02bc44d
    
    Lock screen: Weather panel improvements, (1/2):
    
    - Move weather panel to the bottom the clock panel
    - Add wind
    - Add humidity
    - Add Condition
    - Add Timestamp
    - Add condition icons (colored and VClouds)
    - Rearrange the weather panel:
      - Left side:
        - Location
        - Wind
      - Center:
        - Condiditon icon
      - Right side:
        - Current temperature
        - Humidity
      - Bottom:
        - Condition
        - Timestamp
    - Option to show/hide the timestamp
    - Option to colorize the monochrome icons/all icons
    - Custom text color
    - Custom icon color
    
    Change-Id: I5410340e497cfd6eb359d5da0396e325543fd5d3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -132,6 +132,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.cm.ActionUtils;
+import com.android.internal.util.cm.WeatherControllerImpl;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.internal.util.slim.ActionConfig;
@@ -190,7 +192,6 @@ import com.android.systemui.statusbar.policy.RotationLockControllerImpl;
 import com.android.systemui.statusbar.policy.SecurityControllerImpl;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
-import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 import com.android.systemui.statusbar.policy.ZenModeController;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout.OnChildLocationsChangedListener;
