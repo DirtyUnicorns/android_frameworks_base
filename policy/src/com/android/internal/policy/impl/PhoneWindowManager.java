b870a03ff454350b0e527f077d416884f0bcf232
Henrik Baard <henrik.baard@sonymobile.com>
2015-02-03 09:25:28 +0100

    Remove fall through for KEYCODE_VOICE_ASSIST
    Removing unintentional fallthrough for the case
    KEYCODE_VOICE_ASSIST.
    
    The code works today since KEYCODE_VOICE_ASSIST is the
    last case in the switch statement, however it is bad
    practice. If somone adds another case statement the
    code will break.
    
    Change-Id: Iee6234807bbe176bd94e2584de288105d6c6a7cb

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5691,6 +5691,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     msg.setAsynchronous(true);
                     msg.sendToTarget();
                 }
+                break;
             }
         }
 
