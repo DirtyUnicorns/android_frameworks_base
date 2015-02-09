7175357093da190582ab336d733174e0794c1451
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:39:52 +0000

    Add Battery saver tile [1/2]
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: I7bb419ac5d135b27f9f35d4a7acdb452a197a68f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -34,6 +34,7 @@ import com.android.internal.util.du.QSUtils;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.BrightnessTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -298,6 +299,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenTimeoutTile(this);
             case QSConstants.TILE_BRIGHTNESS:
                 return new BrightnessTile(this);
+            case QSConstants.TILE_BATTERY_SAVER:
+                return new BatterySaverTile(this);
 	    case QSConstants.TILE_HEADS_UP:
 		return new HeadsupTile(this);
             default:
