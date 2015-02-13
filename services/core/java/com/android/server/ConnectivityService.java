ba5433bf6a0da67cf4d610ce422d1f67cd17bf26
Eri Tanaka <eri.x.tanaka@sonymobile.com>
2015-02-13 12:05:40 +0900

    Fix system crash caused by invalid array index of LegacyTypeTracker
    This issue occurs when switching between Cellular and Wi-Fi repeatedly.
    Because ConnectivityService.LegacyTypeTracker which is called by multiple
    threads is not thread-safe, IndexOutOfBoundsException occurs at member
    variable mTypeLists[type].
    For example, at the following step 3, exception occurs by getting value
    from invalid index:
    1. (Thread A) Confirm that mTypeLists[type] of LegacyTypeTracker is not
    empty.
    2. (Thread B) Remove element in mTypeLists[type].
    3. (Thread A) Get element of index 0 from mTypeLists[type].
    
    To mitigate, add synchronization.
    
    Change-Id: Ia963662edb9d643790e8d9439e4dbdcac4c2187b

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -478,7 +478,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                     new ArrayList[ConnectivityManager.MAX_NETWORK_TYPE + 1];
         }
 
-        public void addSupportedType(int type) {
+        public synchronized void addSupportedType(int type) {
             if (mTypeLists[type] != null) {
                 throw new IllegalStateException(
                         "legacy list for type " + type + "already initialized");
@@ -486,11 +486,11 @@ public class ConnectivityService extends IConnectivityManager.Stub
             mTypeLists[type] = new ArrayList<NetworkAgentInfo>();
         }
 
-        public boolean isTypeSupported(int type) {
+        public synchronized boolean isTypeSupported(int type) {
             return isNetworkTypeValid(type) && mTypeLists[type] != null;
         }
 
-        public NetworkAgentInfo getNetworkForType(int type) {
+        public synchronized NetworkAgentInfo getNetworkForType(int type) {
             if (isTypeSupported(type) && !mTypeLists[type].isEmpty()) {
                 return mTypeLists[type].get(0);
             } else {
@@ -508,7 +508,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
 
         /** Adds the given network to the specified legacy type list. */
-        public void add(int type, NetworkAgentInfo nai) {
+        public synchronized void add(int type, NetworkAgentInfo nai) {
             if (!isTypeSupported(type)) {
                 return;  // Invalid network type.
             }
@@ -530,7 +530,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
 
         /** Removes the given network from the specified legacy type list. */
-        public void remove(int type, NetworkAgentInfo nai, boolean wasDefault) {
+        public synchronized void remove(int type, NetworkAgentInfo nai, boolean wasDefault) {
             ArrayList<NetworkAgentInfo> list = mTypeLists[type];
             if (list == null || list.isEmpty()) {
                 return;
@@ -559,7 +559,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
 
         /** Removes the given network from all legacy type lists. */
-        public void remove(NetworkAgentInfo nai, boolean wasDefault) {
+        public synchronized void remove(NetworkAgentInfo nai, boolean wasDefault) {
             if (VDBG) log("Removing agent " + nai + " wasDefault=" + wasDefault);
             for (int type = 0; type < mTypeLists.length; type++) {
                 remove(type, nai, wasDefault);
@@ -568,7 +568,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
         // send out another legacy broadcast - currently only used for suspend/unsuspend
         // toggle
-        public void update(NetworkAgentInfo nai) {
+        public synchronized void update(NetworkAgentInfo nai) {
             final boolean isDefault = isDefaultNetwork(nai);
             final DetailedState state = nai.networkInfo.getDetailedState();
             for (int type = 0; type < mTypeLists.length; type++) {
