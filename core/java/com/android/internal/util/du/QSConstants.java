74ad96e02e4f35b05c2af532c9534732f183140f
Alex Cruz <mazdarider23@gmail.com>
2015-02-20 06:08:14 +0000

    NavBar tile [1/3]
    Change-Id: I85a33bffa76aac4f0efbc86ce68e32858306c025

--- core/java/com/android/internal/util/du/QSConstants.java
+++ core/java/com/android/internal/util/du/QSConstants.java
@@ -52,6 +52,7 @@ public class QSConstants {
     public static final String TILE_COMPASS = "compass";
     public static final String TILE_NFC = "nfc";
     public static final String TILE_ADB_NETWORK = "adb_network";
+    public static final String TILE_NAVBAR = "toggleNavBar";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -94,5 +95,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_COMPASS);
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_ADB_NETWORK);
+        TILES_AVAILABLE.add(TILE_NAVBAR);
     }
 }
