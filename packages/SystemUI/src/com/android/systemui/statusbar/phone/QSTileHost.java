a46d80f96ab0413a6867f013ea58cf6e768e4001
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-02-21 22:52:13 +0100

    Reboot/Recovery tile (1/2)
    Forward port from SlimKat
    
    Change-Id: Ia933ffd48c485a8a6ca72a3c60139b144ac80e90

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
@@ -295,6 +296,8 @@ public class QSTileHost implements QSTile.Host {
                 return new LockscreenToggleTile(this);
             case QSConstants.TILE_SCREENSHOT:
                 return new ScreenshotTile(this);
+            case QSConstants.TILE_REBOOT:
+                return new RebootTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             case QSConstants.TILE_SCREEN_TIMEOUT:
