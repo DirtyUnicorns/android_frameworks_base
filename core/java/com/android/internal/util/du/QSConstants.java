01047c0f0d3801c167478c5b12260db2de1daf8b
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-02-21 22:52:13 +0100

    Reboot/Recovery tile (1/2)
    Forward port from SlimKat
    
    Change-Id: Ia933ffd48c485a8a6ca72a3c60139b144ac80e90

--- core/java/com/android/internal/util/du/QSConstants.java
+++ core/java/com/android/internal/util/du/QSConstants.java
@@ -37,6 +37,7 @@ public class QSConstants {
     public static final String TILE_DDS = "dds";
     public static final String TILE_APN = "apn";
     public static final String TILE_LOCKSCREEN = "lockscreen";
+    public static final String TILE_REBOOT = "reboot";
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_SYNC = "sync";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
@@ -75,6 +76,7 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LOCKSCREEN);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
         TILES_AVAILABLE.add(TILE_SYNC);
+        TILES_AVAILABLE.add(TILE_REBOOT);
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_BRIGHTNESS);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -52,6 +52,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RoamingTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -317,6 +318,8 @@ public class QSTileHost implements QSTile.Host {
                 return new LockscreenToggleTile(this);
             case QSConstants.TILE_SCREENSHOT:
                 return new ScreenshotTile(this);
+            case QSConstants.TILE_REBOOT:
+                return new RebootTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             case QSConstants.TILE_SCREEN_TIMEOUT:
