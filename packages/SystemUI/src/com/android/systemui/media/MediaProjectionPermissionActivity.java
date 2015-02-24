f0688eead307421ecb597019cda432ca4a394c29
John Spurlock <jspurlock@google.com>
2015-02-24 12:14:06 -0500

    MediaProjectionPermissionActivity: Fix NPE.
    Avoid dismissing a null dialog.
    
    Bug: 19492919
    Change-Id: I6642bb5194d9818ff3db1266c7c78ed752e29ecf

--- packages/SystemUI/src/com/android/systemui/media/MediaProjectionPermissionActivity.java
+++ packages/SystemUI/src/com/android/systemui/media/MediaProjectionPermissionActivity.java
@@ -114,6 +114,7 @@ public class MediaProjectionPermissionActivity extends Activity
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mDialog != null) {
         mDialog.dismiss();
     }
 
@@ -128,7 +129,9 @@ public class MediaProjectionPermissionActivity extends Activity
             Log.e(TAG, "Error granting projection permission", e);
             setResult(RESULT_CANCELED);
         } finally {
-            mDialog.dismiss();
+            if (mDialog != null) {
+                mDialog.dismiss();
+            }
             finish();
         }
     }
