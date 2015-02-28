75011340ccab0291f365e924d09d2510bf46a4a8
Jorge Ruesga <jorge@ruesga.com>
2015-02-28 03:14:01 +0100

    Never show excluded recent task in recents view
    Actually, when there there isn't recent tasks to show, the systemui forces to show
    task with the flag FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS, but only the first one.
    This create an non-consistent behaviour when the user remove all recent task and
    then there is a new recent task in the recents view, kill that and a new one
    appears, an so on, until no task are available.
    
    This patch changes that behaviour to:
    - Never show tasks in recents view with the flag FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
    - Clear recents button kill all the tasks (included the ones with FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
    
    Change-Id: I8dc2859ca9dcde7229d5fdb502da313b1ae0dccb

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -21,11 +21,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.os.UserHandle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
+import android.widget.PopupMenu;
+
 import com.android.systemui.R;
 import com.android.systemui.recents.Constants;
 import com.android.systemui.recents.RecentsConfiguration;
@@ -563,6 +566,10 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         mStack.removeTask(t);
                     }
                 }
+
+                // And remove all the excluded or all the other tasks
+                SystemServicesProxy ssp = RecentsTaskLoader.getInstance().getSystemServicesProxy();
+                ssp.removeAllUserTask(UserHandle.myUserId());
             }
         });
     }
