8fbe263c7526447bf5b64eb4fd515eb7eb19e587
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-02 11:46:56 +0000

    Add Ambient display tile [1/3]
    Drawables thanks to Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ia1279d9f2295e6be42fbc1b8cd79b458de7816f9

--- core/java/com/android/internal/util/du/QSConstants.java
+++ core/java/com/android/internal/util/du/QSConstants.java
@@ -45,6 +45,7 @@ public class QSConstants {
     public static final String TILE_HEADS_UP = "toggleHeadsUp";
     public static final String TILE_NAVBAR = "toggleNavBar";
     public static final String TILE_APPCIRCLEBAR = "toggleAppCircleBar";
+    public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -80,5 +81,6 @@ public class QSConstants {
 	TILES_AVAILABLE.add(TILE_HEADS_UP);
         TILES_AVAILABLE.add(TILE_NAVBAR);
         TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
+        TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
     }
 }
