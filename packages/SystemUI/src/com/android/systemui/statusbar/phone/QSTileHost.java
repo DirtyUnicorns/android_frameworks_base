3e8df02facd8be7825f65e750a44575fb1080128
eyosen <abittin@gmail.com>
2015-03-04 22:35:27 +0000

    Screenrecord tile [1/2]
    Change-Id: I2df2ff0c8b81eb77b3d112d357cdf18cb19f4c2b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.HeadsupTile;
@@ -299,6 +300,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenshotTile(this);
             case QSConstants.TILE_REBOOT:
                 return new RebootTile(this);
+            case QSConstants.TILE_SCREENRECORD:
+                return new ScreenrecordTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             case QSConstants.TILE_SCREEN_TIMEOUT:
