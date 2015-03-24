3b2e217cd1eb12e1bd41549d65fad565e3cf67fd
Meticulus <theonejohnnyd@gmail.com>
2015-03-24 15:14:17 +0100

    Live Volume Steps [1/2]
    -Volume steps changes are instant, no need to reboot!
    
    **** Modifications for Marshmallow by DirtyUnicorns ****
    
    -- Broadcast a new intent from AudioService when steps change
    -- Receive broadcast in VolumeController triggering a state
       refresh
    
    Conflicts:
    	media/java/android/media/AudioManager.java
    	media/java/android/media/AudioService.java
    	media/java/android/media/IAudioService.aidl
    
    Change-Id: Ia803a6ef38e5e89e1af9a31e7904d77ec1f16722

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -700,7 +700,8 @@ public class VolumeDialog {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -757,7 +758,7 @@ public class VolumeDialog {
         // update slider value - 0 if silent or vibrate
         final int vlevel = row.ss.muted && (isNotificationSilent || isRingVibrate && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowHeaderVisibleH(VolumeRow row) {
@@ -781,7 +782,7 @@ public class VolumeDialog {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -805,7 +806,7 @@ public class VolumeDialog {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
