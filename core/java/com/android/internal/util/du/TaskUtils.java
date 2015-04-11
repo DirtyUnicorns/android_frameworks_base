810a3cc3b75ebbe0ef8d1a538ca41e79924bb9ab
Alex Cruz <mazdarider23@gmail.com>
2015-04-11 01:13:34 +0000

    Remove task kill process

--- core/java/com/android/internal/util/du/TaskUtils.java
+++ core/java/com/android/internal/util/du/TaskUtils.java
@@ -133,16 +133,6 @@ public class TaskUtils {
         return -1;
     }
 
-    public static void killPackageProcess(int mId, Context context) {
-
-        Context mContext = context;
-
-        final ActivityManager am = (ActivityManager) mContext
-            .getSystemService(Context.ACTIVITY_SERVICE);
-
-        am.removeTask(mId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
-    }
-
     public static void movePackageToFront(int mId, Context context) {
 
         Context mContext = context;
