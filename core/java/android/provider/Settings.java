ae4f35a7489745186ddd4ecd9d74f4109b7fef1b
Stevespear426 <stephen.k.spear@gmail.com>
2015-05-10 20:59:20 -0400

    AOKP custom system animations (1/3)
    Change-Id: Iee39208de6b33d4bbfaebe3210e73f57dfa04ea7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3660,6 +3660,25 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
+         * AOKP Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
+        /**
          * Custom button brightness value for manual mode
          *
          * @hide
@@ -8299,6 +8318,7 @@ public final class Settings {
           * @hide
           */
         public static final String TUNE_AWAY_STATUS = "tune_away";
+
     }
 
     /**
