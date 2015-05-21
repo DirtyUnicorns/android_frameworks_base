8ad24fc3469a36609277bac596cb77f4529cade0
Daniel Koman <dankoman30@gmail.com>
2015-05-20 23:38:10 -0400

    Fix show menu never not working
    Change-Id: I646542f9dc25a672d1413035aa7831de05bdf030

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -102,6 +102,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     private int mMenuVisibility;
     private int mMenuSetting;
     private boolean mOverrideMenuKeys;
+    private boolean mIsImeButtonVisible = false;
 
     final Display mDisplay;
     View mCurrentView = null;
@@ -671,6 +672,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
         final boolean showImeButton = ((hints & StatusBarManager.NAVIGATION_HINT_IME_SHOWN) != 0);
         if (getImeSwitchButton() != null)
             getImeSwitchButton().setVisibility(showImeButton ? View.VISIBLE : View.GONE);
+            mIsImeButtonVisible = showImeButton;
 
         // Update menu button in case the IME state has changed.
         setMenuVisibility(mShowMenu, true);
@@ -792,7 +794,8 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
             return;
         } else if (mMenuVisibility == MENU_VISIBILITY_NEVER) {
             leftMenuKeyView.setVisibility(View.INVISIBLE);
-            rightMenuKeyView.setVisibility(View.INVISIBLE);
+            rightMenuKeyView.setVisibility(
+                    mIsImeButtonVisible ? View.GONE : View.INVISIBLE);
         }
 
 
@@ -806,7 +809,8 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                 && shouldShow;
 
         leftMenuKeyView.setVisibility(showLeftMenuButton ? View.VISIBLE : View.INVISIBLE);
-        rightMenuKeyView.setVisibility(showRightMenuButton ? View.VISIBLE : View.INVISIBLE);
+        rightMenuKeyView.setVisibility(showRightMenuButton ? View.VISIBLE
+                : (mIsImeButtonVisible ? View.GONE : View.INVISIBLE));
         mShowMenu = show;
     }
 
