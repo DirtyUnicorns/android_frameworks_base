7eced4eb2b4fbbfe36c76a0d466c33bfb9524b6f
Alex Cruz <mazdarider23@gmail.com>
2015-05-30 18:30:25 +0000

    Add missing import
    Change-Id: I955fbef454102eab3ba11b7b94565cbe733816c1

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -177,6 +177,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings.Global;
+import android.provider.Settings.Secure;
 import android.security.KeyStore;
 import android.security.SystemKeyStore;
 import android.system.ErrnoException;
