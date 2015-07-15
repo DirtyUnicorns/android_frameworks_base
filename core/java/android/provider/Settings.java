dc38f463107856cbd0e2720f0df4e910b07d35f8
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-07-16 00:50:48 +0200

    Slimified CM Navigation Ring Targets [1/2]
    Author : Danesh M <daneshm90@gmail.com>
    
    Source :
    
    http://review.cyanogenmod.org/#/c/87164/5
    http://review.cyanogenmod.org/#/c/85439/7 (partially, only resources)
    http://review.cyanogenmod.org/#/c/88000/2
    
    PS1 Changes added :
    
    - allow disabling NavRings completely (Yank555.lu)
    
    PS3 Changes added :
    
    - better icon choices for killtask and empty (dankoman)
    
    PS4 Changes added :
    
    - replace NavRing actions by SlimActions (kagwind)
    - use only valid subset of SlimActions (Yank555.lu)
    - recolor SlimAction drawables so they are visible (Yank555.lu)
    
    Change-Id: Ibbb815e8e5975b68f5e4b2e4433897e406de40bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1248,6 +1248,11 @@ public final class Settings {
 
             // At one time in System, then Global, but now back in Secure
             MOVED_TO_SECURE.add(Secure.INSTALL_NON_MARKET_APPS);
+
+            MOVED_TO_SECURE.add(Secure.SEARCH_PANEL_ENABLED);
+            for (String s : Secure.NAVIGATION_RING_TARGETS) {
+                MOVED_TO_SECURE.add(s);
+            }
         }
 
         private static final HashSet<String> MOVED_TO_GLOBAL;
@@ -6486,6 +6491,22 @@ public final class Settings {
          * @hide
          */
         public static final String ADB_PORT = "adb_port";
+
+        /**
+         * Custom navring actions
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+                "navigation_ring_targets_0",
+                "navigation_ring_targets_1",
+                "navigation_ring_targets_2",
+        };
+
+        /**
+         * Whether search panel is enabled.
+         * @hide
+         */
+        public static final String SEARCH_PANEL_ENABLED = "search_panel_enabled";
 
         /**
          * This are the settings to be backed up.
