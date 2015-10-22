ce46927e65599047b4e1eb1bb7ba112f2dd1ba49
camcory <camcory1@gmail.com>
2015-10-22 18:00:34 -0400

    Correct QS Tuner reset
    Have the tiles reset to the new defined values in config "quick_settings_tiles"
    
    Change-Id: Ia7923030c5e5eb760766b1ff8a5525cc28114eed

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -257,8 +257,9 @@ public class QsTuner extends Fragment implements Callback {
         }
 
         public void reset() {
-            Secure.putStringForUser(getContext().getContentResolver(),
-                    TILES_SETTING, "default", ActivityManager.getCurrentUser());
+            Secure.putStringForUser(getContext().getContentResolver(), TILES_SETTING,
+                    "wifi,bt,dnd,cell,airplane,rotation,flashlight,location,cast",
+                    ActivityManager.getCurrentUser());
         }
 
         private void setTiles(List<String> tiles) {
