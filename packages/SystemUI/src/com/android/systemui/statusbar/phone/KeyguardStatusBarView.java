10bec208ad21d452d621bb703e3ed1ec8f59a219
Alex Cruz <mazdarider23@gmail.com>
2015-11-16 11:55:39 +0000

    Fixed issue with battery text not showing up in the keyguard
    Change-Id: I2359757dfec301e747a865bf4a4058934e4514b4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -95,6 +95,7 @@ public class KeyguardStatusBarView extends RelativeLayout {
         mFastOutSlowInInterpolator = AnimationUtils.loadInterpolator(getContext(),
                 android.R.interpolator.fast_out_slow_in);
         updateUserSwitcher();
+        updateVisibilities();
     }
 
     @Override
