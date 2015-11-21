1a01cf3ae0903184c9cdf6e8c4a4e843f3c4de4a
maxwen <max.weninger@gmail.com>
2015-11-21 02:00:46 +0000

    Add support for adb over network [1/2]
    Change-Id: Ifbed5500178a16d47203af6c8c7e5fb35d42bb88

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6582,6 +6582,12 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
