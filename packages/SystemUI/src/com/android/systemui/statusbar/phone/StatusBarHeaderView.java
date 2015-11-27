a2e10266b921d3faa47f53707e0da8052b12e8d1
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-11-27 05:55:47 +0000

    Lockscreen weather options [1/2]
    Change-Id: Iac685324f61af102c88e4aff8d7d462ff4557cdb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -55,6 +55,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.internal.util.du.WeatherController;
+import com.android.internal.util.du.WeatherControllerImpl;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryLevelTextView;
@@ -68,8 +70,6 @@ import com.android.systemui.statusbar.policy.NetworkControllerImpl.EmergencyList
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.tuner.TunerService;
-import com.android.systemui.statusbar.policy.WeatherController;
-import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 
 import java.text.NumberFormat;
 
