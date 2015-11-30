94d9db0e06e66030a0eea73ff524a2aec920985d
Christian Morlok <ChristianMorlok@gmail.com>
2015-11-30 21:03:47 +0000

    Hide power menu on lockscreen [1/2]
    Change-Id: I108742d658d5a7babb8e9d1fa5ed51ae1cec3128

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3193,6 +3193,13 @@ public final class Settings {
         public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
 
         /**
+         * Whether to display powermenu on secure lockscreen
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_LOCKSCREEN = "power_menu_lockscreen";
+
+        /**
          * Default screen rotation when no other policy applies.
          * When {@link #ACCELEROMETER_ROTATION} is zero and no on-screen Activity expresses a
          * preference, this rotation value will be used. Must be one of the
