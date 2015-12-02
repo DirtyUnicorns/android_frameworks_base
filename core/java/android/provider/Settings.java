536fe1debc3ada84c705ff446f7fea4f2646d97f
Zeeshan <zeetherocker@gmail.com>
2015-12-02 09:41:15 +0000

    Support for Configurable Trigger Region
    Change-Id: I16c15a158a127c0c6201c2d01b76a8885098024b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4134,6 +4134,34 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
+         * Width of the app circle bar trigger
+         *
+         * @hide
+         */
+        public static final String APP_CIRCLE_BAR_TRIGGER_WIDTH = "app_circle_bar_trigger_width";
+
+        /**
+         * Position of app circle bar trigger
+         *
+         * @hide
+         */
+        public static final String APP_CIRCLE_BAR_TRIGGER_TOP = "app_circle_bar_trigger_top";
+
+        /**
+         * Height of the app circle bar trigger
+         *
+         * @hide
+         */
+        public static final String APP_CIRCLE_BAR_TRIGGER_HEIGHT = "app_circle_bar_trigger_height";
+
+        /**
+         * Whether to display the trigger region or not
+         *
+         * @hide
+         */
+        public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
+
+        /**
          * Whether to enable status and navigation bar color in battery saver mode.
          *
          * @hide
