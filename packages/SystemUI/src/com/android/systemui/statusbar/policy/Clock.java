5efcfb51985f67baea7d03ea2925d18cd4997ef0
Randall Rushing <randall.rushing@gmail.com>
2015-12-08 00:26:41 -0800

    statusbar: fix notification icons bleeding into center clock view
    -- Pass StatusbarIconController to Clock instances. mIconController
       was declared in Clock, but was never set.
    
    -- Include DULogo and status bar Carrier label in IconMerger
       width calculation
    
    -- Move "more" icon to the right of IconMerger. This is more inline
       with AOSP. Also, it seems we want status bar carrier label
       persistently aligned to the far left of the status bar
    
    Change-Id: I1256f98b00c154b38371a52e152d20c687772f02

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -153,6 +153,10 @@ public class Clock extends TextView implements DemoMode {
         }
     }
 
+    public void setIconController(StatusBarIconController iconController) {
+        mIconController = iconController;
+    }
+
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
