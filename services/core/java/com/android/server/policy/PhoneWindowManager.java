537a0e0dcce32beeabb2c45c529a466a48d220f2
Randall Rushing <randall.rushing@gmail.com>
2015-12-09 00:45:02 -0800

    Actions: Turn on screen record bindable action [2/2]
    Change-Id: I87a0cd019fc6516442fc338a3c85601516547b24

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -747,6 +747,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             } else if (ActionHandler.INTENT_SCREENSHOT.equals(action)) {
                 mHandler.removeCallbacks(mScreenshotRunnable);
                 mHandler.post(mScreenshotRunnable);
+            } else if (ActionHandler.INTENT_TOGGLE_SCREENRECORD.equals(action)) {
+                mHandler.removeCallbacks(mScreenrecordRunnable);
+                mHandler.post(mScreenrecordRunnable);
             }
         }
     };
@@ -1671,6 +1674,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         filter = new IntentFilter();
         filter.addAction(ActionHandler.INTENT_SHOW_POWER_MENU);
         filter.addAction(ActionHandler.INTENT_SCREENSHOT);
+        filter.addAction(ActionHandler.INTENT_TOGGLE_SCREENRECORD);
         context.registerReceiver(mDUReceiver, filter);
 
         // monitor for system gestures
