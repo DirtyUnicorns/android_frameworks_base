c79d30bda902234efb49ddbc7f257e9dc9d509af
Nathan Benis <nbenis106@gmail.com>
2016-01-03 10:16:03 +0000

    Disable Lockscreen Media Art [1/2]
    * toggle to choose whether or not to show album art/media art on lockscreen
    
    Special thanks to @atl4tis from Cataclysm rom for pointing out where to hook into this
    
    Change-Id: I4fe25c54763d66f61f75b5c6b678e9fa095e3333

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2377,6 +2377,12 @@ public final class Settings {
             "lock_pattern_tactile_feedback_enabled";
 
         /**
+         * Whether to show media art on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Whether to scramble a pin unlock layout
          * @hide
          */
