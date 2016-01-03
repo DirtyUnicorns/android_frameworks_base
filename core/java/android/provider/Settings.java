d4b76163034bc8795a9d1a80f9151c570d5ecba1
Alex Cruz <mazdarider23@gmail.com>
2016-01-03 04:07:47 +0000

    Lockscreen Clock Widget Fonts [1/2]
    This is @beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    Change-Id: I3dccd912166643645b2b97017a7a267649ab6538

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3857,6 +3857,13 @@ public final class Settings {
         public static final String RECENTS_FULL_SCREEN = "recents_full_screen";
 
         /**
+         * Change fonts for the system lockscreen clock widget
+         *
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Whether to display set of animations when prompting the power menu
          *
          * @hide
