5139e5790ca515b47a58ea440e44f47563fdccca
George G <kreach3r@users.noreply.github.com>
2016-01-12 05:08:51 +0200

    Fix headsup notification setting (peeking) resetting on reboot
    Steps to reproduce the bug:
    1. Go into any app's App info
    2. Toggle only "allow peeking" (true --> false or false --> true, just change it)
    3. Reboot
    4. The setting value is reset to the previous value (turns out, it was never saved)
    
    Change-Id: Iacd8aa08397131d48e60185924fe5016c6b8bbed

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1351,8 +1351,8 @@ public class NotificationManagerService extends SystemService {
         @Override
         public void setPackagePeekable(String pkg, int uid, boolean peekable) {
             checkCallerIsSystem();
-
             mRankingHelper.setPackagePeekable(pkg, uid, peekable);
+            savePolicyFile();
         }
 
         @Override
