bb32ce0cac24b53d94479db792ea41bce52bf20f
Keith Mok <kmok@cyngn.com>
2016-02-04 20:10:00 +0100

    SystemUI: Add Caffeine QS tile
    *DU Edits*
    - Adopt to P style tiles and utilize just one icon.
    - Removed unused imports and strings
    
    Change-Id: I03bfc8c9bae4a4be92224c1eccbae0d58b0b7769

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -26,6 +26,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
