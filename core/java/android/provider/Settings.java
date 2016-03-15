fcc7c462f01d8299fec08457419a5bc57119485a
Willi Ye <williye97@gmail.com>
2016-03-15 21:11:47 +0100

    Swap 'Back' and 'Recents' hw keys
    Change-Id: Idc8c69ab1ccea44ae24fa5618a2cf2b3bf2aff21

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4677,6 +4677,14 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * Whether user can swap navigation lateral keys.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
