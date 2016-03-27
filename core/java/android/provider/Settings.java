eb78eee586bfd2780f816267730dc5180b2420e8
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    Heads Up: Add timeout option (1/2)
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: If542480dc1efd4d92ba1d536fa23d0342569b8e1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4331,6 +4331,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
