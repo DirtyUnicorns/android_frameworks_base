c0d1b88277c45c8959db2e7de57af745210efa74
Randall Rushing <randall.rushing@gmail.com>
2016-04-18 01:56:02 -0700

    Use CircularReveal animation to show/hide task manager panel
    Use the QSDetailClipper class to provide circular reveal when
    transitioning between QSPanel and TaskManager panel. The show/hide
    reveal animation isn't quite flawless when there is a large
    difference in panel size. In almost all usecases however, this
    is cleaner than the rough visibility change we have now.
    
    Change-Id: I24b8061613cbc0a1b7ab8cfabc4f6055eeb7458e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -62,6 +62,7 @@ import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSContainer;
+import com.android.systemui.qs.QSDetailClipper;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.statusbar.ExpandableNotificationRow;
 import com.android.systemui.statusbar.ExpandableView;
@@ -226,6 +227,7 @@ public class NotificationPanelView extends PanelView implements
     private Handler mHandler = new Handler();
     private SettingsObserver mSettingsObserver;
     private int mQsSmartPullDown;
+    private QSDetailClipper mClipper;
 
     private Runnable mHeadsUpExistenceChangedRunnable = new Runnable() {
         @Override
@@ -266,6 +268,7 @@ public class NotificationPanelView extends PanelView implements
         mQsContainer = (QSContainer) findViewById(R.id.quick_settings_container);
         mQsPanel = (QSPanel) findViewById(R.id.quick_settings_panel);
         mTaskManagerPanel = (LinearLayout) findViewById(R.id.task_manager_panel);
+        mClipper = new QSDetailClipper(mTaskManagerPanel);
         mClockView = (TextView) findViewById(R.id.clock_view);
         mScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
         mScrollView.setListener(this);
@@ -1564,16 +1567,25 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void setTaskManagerVisibility(boolean taskManagerShowing) {
-        if (mShowTaskManager) {
+        if (mShowTaskManager && !mKeyguardShowing) {
             mTaskManagerShowing = taskManagerShowing;
             cancelAnimation();
-            boolean expandVisually = mQsExpanded || mStackScrollerOverscrolling;
-            mQsPanel.setVisibility(expandVisually && !taskManagerShowing
-                    ? View.VISIBLE : View.GONE);
-            mTaskManagerPanel.setVisibility(expandVisually && taskManagerShowing
-                    && !mKeyguardShowing ? View.VISIBLE : View.GONE);
+            int x = mQsPanel.getLeft() + mQsPanel.getWidth() / 2;
+            int y = mQsPanel.getTop() + mQsPanel.getHeight() / 2;
+            if (!taskManagerShowing) {
+                mQsPanel.setVisibility(View.VISIBLE);
+            }
+            mClipper.animateCircularClip(x, y, taskManagerShowing, mHideQsPanelWhenDone);
         }
     }
+
+    private final AnimatorListenerAdapter mHideQsPanelWhenDone = new AnimatorListenerAdapter() {
+        public void onAnimationEnd(Animator animation) {
+            if (mTaskManagerShowing) {
+                mQsPanel.setVisibility(View.GONE);
+            }
+        };
+    };
 
     private void cancelAnimation() {
         if (mQsExpansionAnimator != null) {
