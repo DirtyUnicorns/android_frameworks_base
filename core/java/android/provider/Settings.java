e193a3705b2b2523571fe8a45c56972d9576658f
Alex <mazdarider23@gmail.com>
2016-09-12 21:41:56 -0400

    Remove dashboard tile summaries [2/2]
    Change-Id: I028523a50949741d11e61d88cd50891404a52b26

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3626,6 +3626,12 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
         /**
+         * Remove tile summaries in settings
+         * @hide
+         */
+        public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
+
+        /**
          * Volume rocker wake
          * @hide
          */
