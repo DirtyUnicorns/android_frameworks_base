2096a8128cdf4986225aa8ff21091954c596140f
Christian Oder <myself5@carbonrom.org>
2016-09-13 16:11:42 +0200

    fwb: Fix reboot when hot reboot was selected once
    If you select hot reboot, and then something else, the device will still
    do a hot reboot, as mRebootHot is still true.
    
    Only happens to people with fat fingers like me, which keep on selecting
    the wrong thing, but a fix doesn't hurt though.
    
    Change-Id: If5494fdf67ca0ec7557939769a02a2fe5b31617d

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -206,6 +206,8 @@ public final class ShutdownThread extends Thread {
                                         mReason = actions[which];
                                         if (actions[which].equals(HOT_REBOOT)) {
                                             mRebootHot = true;
+                                        } else {
+                                            mRebootHot = false;
                                         }
                                     }
                                 }
