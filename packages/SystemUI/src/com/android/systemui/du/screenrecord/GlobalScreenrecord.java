614177319443d18e990ccda1c5db94de2931d831
Alex Cruz <mazdarider23@gmail.com>
2016-10-19 07:41:22 +0200

    Move everything to one folder, little house cleaning
    Change-Id: If950a51b41f399da6bfb7241070d2175191c068e

--- packages/SystemUI/src/com/android/systemui/du/screenrecord/GlobalScreenrecord.java
+++ packages/SystemUI/src/com/android/systemui/du/screenrecord/GlobalScreenrecord.java
@@ -29,7 +29,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package com.android.systemui.omni.screenrecord;
+package com.android.systemui.du.screenrecord;
 
 import android.app.Notification;
 import android.app.NotificationManager;
