638677c02abe808485aa059bab5164b55a88bf8e
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-23 05:26:54 -0500

    Battery tile: allow to disable custom battery style [1/2]
    Change-Id: Iddb6a99a158b6f70d74e30a71766e94eeae30adb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7120,6 +7120,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Whether to sync the battery tile stile with the statusbar battery icon style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE_TILE = "status_bar_battery_style_tile";
+
+        /**
          * Twilight mode always off.
          * @hide
          */
@@ -7299,7 +7305,8 @@ public final class Settings {
             ADVANCED_REBOOT,
             QS_TILES,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            STATUS_BAR_BATTERY_STYLE_TILE
         };
 
         /**
