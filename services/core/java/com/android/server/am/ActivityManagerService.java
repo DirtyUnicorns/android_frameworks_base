4381a05a2e1db0e0fe40a3f7ae4758c64a8cb625
Shasidhar Ganiga <sganig@codeaurora.org>
2017-01-09 11:32:34 +0530

    Fix InCallActivity onStop call when launch from Lockscreen
    Phone Account Selection Dialog is displayed over lockscreen for
    an outgoing call. But since the dialog gets onStop from framework,
    the dialog and hence the call gets disconnected. This is due to
    delay in sleeping state in framework as per current lock screen
    design
    Current Fix tries to determine the sleeping state not just based
    on the state variable but also on the wakefulness state of the
    device to determine sleeping state.
    
    Test: When calling account set as Ask Every time get miscall and
    lock screen initiate call by clicking on Callback button, sim
    selection dialog should be shown and after selecting phone
    account call should be initiated.
    Test: When calling account set as first phone account call should
    initiate directly
    Test: In unlocked screen When calling account set as Ask Every
    time get miscall initiate call from Dialer, sim selection dialog
    should be shown and after selecting phone account call should be
    initiated.
    Test: In unlocked screen When calling account set as Ask Every
    time get miscall initiate call from Dialer, sim selection dialog
    should be shown and clicking outside should remove the call
    instance created.
    Test: Normal calling cases like MO/MT calls should work
    Test: Video calls with rotation changes
    Test: monkey testing
    
    Change-Id: Ie7db7eb695915308fe7b1ef658188e9e54625990
    Signed-off-by: Shasidhar Ganiga <sganig@codeaurora.org>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -11617,7 +11617,7 @@ public final class ActivityManagerService extends ActivityManagerNative
     }
 
     boolean isSleepingLocked() {
-        return mSleeping;
+        return mSleeping && mWakefulness == PowerManagerInternal.WAKEFULNESS_ASLEEP;
     }
 
     void onWakefulnessChanged(int wakefulness) {
