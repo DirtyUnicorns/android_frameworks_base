a0ed0d4abe265876df0158a28aed63468e782f7f
ezio84 <brabus84@gmail.com>
2017-01-12 19:07:04 +0100

    QS: Smart Ambient-LiftToWake tile [1/2]
    Useful when cleaning up the device screen :D
    
    Now that N makes available two separate option for doze notifications
    (display-ambient and Gestures-LiftToWake) we need to handle them both
    with the tile.
    It will act this way:
    1) if any of the two options is enabled, and you tap the tile,
    both options are disabled so you can clean the screen or avoid
    unwanted disturbing wakes;
    2) if you tap the tile again, the tile will restore, separately,
    the two options state you had before disabling them;
    3) if you manually disable the two options, the tile will go to
     disabled state;
    4) if both options (thus the tile) are disabled, and you manually enable
    on of them, the tile will go to enabled state and act again as 1) and 2)
    
    Thanks Alex Cruz (@Mazda-- ) for the icons!
    
    Change-Id: I344a254faec9ba6ae47d3afceeb84f8175fec097

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.external.TileLifecycleManager;
 import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AmbientLiftToWakeTile;
 import com.android.systemui.qs.tiles.AndroidAutoTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -455,6 +456,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
         else if (tileSpec.equals("androidauto")) return new AndroidAutoTile(this);
+        else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
