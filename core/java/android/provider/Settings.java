831295cf311f7117c2e31be5548f0ece0a817d10
Alex Cruz <mazdarider23@gmail.com>
2017-01-21 10:24:22 +0100

    Dashboard summaries double lines toggle [1/2]
    Change-Id: Ie0244eb99da54fde7f9fee646614afafc7cd59a6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4002,6 +4002,12 @@ public final class Settings {
         public static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
         /**
+         * Whether to show dashboard summaries in a single line or not
+         * @hide
+         */
+        public static final String DASHBOARD_SUMMARY_DOUBLE_LINES = "dashboard_summary_double_lines";
+
+        /**
          * Remove tile summaries in settings
          * @hide
          */
