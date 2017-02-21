520832474a8e3c543a8439df94f4e919f6f254f1
Alex Cruz <du.alexcruz@gmail.com>
2017-02-21 03:37:10 +0100

    Add IME Switcher icon back into the statusbar
    - hide it from the navbar for a better UI
    
    See image - http://i.imgur.com/bOlllMS.png
    
    Change-Id: I1eb2f2e26898b9e44702550a793efc57eb395d77

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -1851,18 +1851,11 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                 mImeSwitcherNotification.setContentTitle(title)
                         .setContentText(summary)
                         .setContentIntent(mImeSwitchPendingIntent);
-                try {
-                    if ((mNotificationManager != null)
-                            && !mIWindowManager.hasNavigationBar()) {
-                        if (DEBUG) {
-                            Slog.d(TAG, "--- show notification: label =  " + summary);
-                        }
-                        mNotificationManager.notifyAsUser(null,
-                                com.android.internal.R.string.select_input_method,
-                                mImeSwitcherNotification.build(), UserHandle.ALL);
-                        mNotificationShown = true;
-                    }
-                } catch (RemoteException e) {
+                if (mNotificationManager != null) {
+                    mNotificationManager.notifyAsUser(null,
+                            com.android.internal.R.string.select_input_method,
+                            mImeSwitcherNotification.build(), UserHandle.ALL);
+                    mNotificationShown = true;
                 }
             } else {
                 if (mNotificationShown && mNotificationManager != null) {
