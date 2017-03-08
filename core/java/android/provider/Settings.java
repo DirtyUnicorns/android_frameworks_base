6d364b2c187733ce190531ac10361914f9c8b634
George G <kreach3r@users.noreply.github.com>
2017-03-08 15:12:15 +0100

    Force authorize all calling packages by Masquerade [2/3]
    Change-Id: I734eb7404b1f9b41bd47fea386d7805fbe872ffb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7552,6 +7552,12 @@ public final class Settings {
         public static final String WEB_ACTION_ENABLED = "web_action_enabled";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by Masquerade
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * Count of small QS tiles.
          * @hide
          */
