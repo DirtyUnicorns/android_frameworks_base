ba405a4b82b2d56c65340c7349aa75b32e55d122
LorDClockaN <lordclockan@gmail.com>
2017-07-21 09:15:21 +0200

    Hide dui custom icon size settings api
    Change-Id: Icb24e60b9f521ca77e8d93201f3f8c7426bf704d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6029,6 +6029,7 @@ public final class Settings {
 
         /**
          * Scaling value for smartbar custom button icon
+         * @hide
          */
         public static final String SMARTBAR_CUSTOM_ICON_SIZE = "smartbar_custom_icon_size";
 
