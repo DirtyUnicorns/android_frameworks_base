38d1251a5982dd57d4ca95dfb9e6060d50a7dde0
bigrushdog <randall.rushing@gmail.com>
2017-09-06 13:09:52 +0200

    Add API to take screenshots
    * Extracted from DUI
    * Add a public call to Utils
    * Adapt for partial screenshot
    
    Change-Id: I15a8476dfd1c0726d7591acb707586d466c6a124

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7806,4 +7806,9 @@ public class WindowManagerService extends IWindowManager.Stub
                     0 /* configChanges */, !PRESERVE_WINDOWS, true /* notifyClients */);
         }
     }
+
+    @Override
+    public void sendCustomAction(Intent intent) {
+        mPolicy.sendCustomAction(intent);
+    }
 }
