a6a92d3ea15511605da375280cf4360249c8f4ae
Alex Cruz <du.alexcruz@gmail.com>
2018-01-30 23:18:56 -0500

    Require unlocking for more QS tiles
    Taking inspiration from @ezio84's commit below, I feel like the reboot/recovery QS tile,
    Wifi tile and the hotspot one should require unlocking the device on a secured lockscreen
    before using theme.
    
    The whole point of locking up your device is so that people don't mess with it or the setup.
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/9774af33cd85ef976f9b0e4f32c8ab5f1d57480a
    
    Reworked the Reboot/Recovery QS tile with how we reboot the system or into recovery. Thanks @xyyx
    
    Change-Id: I33069727d9de0b000883c04834fd742dcfb0d95b

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -20,20 +20,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.systemui.Dependency;
 import com.android.systemui.R;
+import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.internal.statusbar.IStatusBarService;
+import com.android.systemui.statusbar.policy.KeyguardMonitor;
 
 public class RebootTile extends QSTileImpl<BooleanState> {
 
     private boolean mRebootToRecovery = false;
+    private IStatusBarService mBarService;
+
+    private final ActivityStarter mActivityStarter;
+    private final KeyguardMonitor mKeyguardMonitor;
+    private final Callback mCallback = new Callback();
+
+    private boolean mListening;
 
     public RebootTile(QSHost host) {
         super(host);
+        mActivityStarter = Dependency.get(ActivityStarter.class);
+        mKeyguardMonitor = Dependency.get(KeyguardMonitor.class);
     }
 
     @Override
@@ -50,12 +66,24 @@ public class RebootTile extends QSTileImpl<BooleanState> {
     @Override
     protected void handleLongClick() {
         mHost.collapsePanels();
+        mBarService = IStatusBarService.Stub.asInterface(
+                ServiceManager.getService(Context.STATUS_BAR_SERVICE));
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                PowerManager pm =
-                    (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                pm.reboot(mRebootToRecovery ? "recovery" : "");
+                if (mKeyguardMonitor.isSecure() && !mKeyguardMonitor.canSkipBouncer()) {
+                    mActivityStarter.postQSRunnableDismissingKeyguard(() -> {
+                        MetricsLogger.action(mContext, getMetricsCategory(), !mState.value);
+                        try {
+                            if (mRebootToRecovery)
+                                mBarService.advancedReboot(PowerManager.REBOOT_RECOVERY);
+                            else
+                                mBarService.reboot(false);
+                        } catch (RemoteException e) {
+                        }
+                    });
+                    return;
+                }
             }
         }, 500);
     }
@@ -77,7 +105,7 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRebootToRecovery) {
+       if (mRebootToRecovery) {
             state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
             state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
         } else {
@@ -86,7 +114,20 @@ public class RebootTile extends QSTileImpl<BooleanState> {
         }
     }
 
-    @Override
     public void handleSetListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+        if (listening) {
+            mKeyguardMonitor.addCallback(mCallback);
+        } else {
+            mKeyguardMonitor.removeCallback(mCallback);
+        }
     }
+
+    private final class Callback implements KeyguardMonitor.Callback {
+        @Override
+        public void onKeyguardShowingChanged() {
+            refreshState();
+        }
+    };
 }
