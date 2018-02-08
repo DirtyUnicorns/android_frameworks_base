1caa14eb31fdc9bb0316db530b7f0688fa58710b
Alex Cruz <du.alexcruz@gmail.com>
2018-02-08 01:36:55 -0500

    Add a one time QS dialog for the caffeine tile
    So why this dialog?
    
    Because users are often confused by what this tile actually does. They 
    can figure out what the BT or WiFi tile is for but they can't figure out 
    what this one is for. 
    
    To their credit, the name of the tile doesn't really help identify what 
    the tile is for either. So instead of having to answer the question of 
    'what is this tile' a million times, we're using what we have around us. 
    We're adding a Dialog to hopefully save us from having to answer that 
    question.
    
    Normally I would be against something like this, since we removed both 
    the mobile data and data saver dialogs but I think this one is actually 
    useful unlike the data saver one.
    
    Change-Id: I3423a057baf660009b2a6cb7838fa5c8990e25c7

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.content.DialogInterface;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -29,10 +30,12 @@ import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
+import com.android.systemui.Prefs;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.statusbar.phone.SystemUIDialog;
 
 /** Quick settings tile: Caffeine **/
 public class CaffeineTile extends QSTileImpl<BooleanState> {
@@ -67,7 +70,26 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleClick() {
-        // toggle
+        if (Prefs.getBoolean(mContext, Prefs.Key.QS_CAFFEINE_DIALOG_SHOWN, false)) {
+            drinkUp();
+            return;
+        }
+        SystemUIDialog dialog = new SystemUIDialog(mContext);
+        dialog.setTitle(R.string.caffeine_info_title);
+        dialog.setMessage(R.string.caffeine_info_message);
+        dialog.setPositiveButton(com.android.internal.R.string.ok,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        drinkUp();
+                        Prefs.putBoolean(mContext, Prefs.Key.QS_CAFFEINE_DIALOG_SHOWN, true);
+                    }
+                });
+        dialog.setShowForAllUsers(true);
+        dialog.show();
+    }
+
+    public void drinkUp() {
         if (mWakeLock.isHeld()) {
             mWakeLock.release();
         } else {
