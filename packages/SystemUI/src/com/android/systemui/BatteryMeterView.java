667bf206b107d061addbe4d34fa6db1918afa59d
ezio84 <brabus84@gmail.com>
2018-02-14 15:58:15 +0100

    Statusbar battery text style: show a charging symbol near percentage
    Change-Id: I11a51f2a61fb41e7a21af003fbd7549b436f02f9

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -88,6 +88,8 @@ public class BatteryMeterView extends LinearLayout implements
     public static final int STYLE_CLOCK_RIGHT = 0;
     public static final int STYLE_CLOCK_LEFT = 1;
 
+    private boolean mCharging;
+
     public BatteryMeterView(Context context) {
         this(context, null, 0);
     }
@@ -177,6 +179,7 @@ public class BatteryMeterView extends LinearLayout implements
 
     @Override
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+        mCharging = pluggedIn;
         mDrawable.setBatteryLevel(level);
         mDrawable.setCharging(pluggedIn);
         mLevel = level;
@@ -198,7 +201,12 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void updatePercentText() {
         if (mBatteryPercentView != null) {
-            mBatteryPercentView.setText(
+            // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+            // to load its emoji colored variant with the uFE0E flag
+            String bolt = "\u26A1\uFE0E";
+            CharSequence chargeIndicator =
+                    mCharging && mBatteryIconStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? (bolt + " ") : "";
+            mBatteryPercentView.setText(chargeIndicator +
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
         }
     }
