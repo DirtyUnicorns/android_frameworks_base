4d4e51bf028ff68e938871693f3a72a0be4b2618
Ivan Iskandar <ivan@prjkt.io>
2018-03-19 20:12:10 -0400

    Reload more assets on overlay change
    Complete the current feature of overlay change assets reloads
    implemented with 987f1937a0231a2b6d09bf7c3bbb7df9de04d6b4
    which miss the notification and other small bits in SystemUI
    to be reloaded.
    
    However, this commit will not fully eliminate the need of
    SystemUI restart when changing framework or SystemUI overlay
    since there is more undiscovered bits that can't/doesn't get
    reloaded for some kind of overlay.
    
    @jhenrique09 edits:
    * To prevents lags, don't use 'onOverlayChanged' method
    * Disable statusbar when theme overlay is updating
    * Fix NPE on SlashDrawable
    
    DU edits:
    - add back onOverlayChanged from original subs commit
    old notifications get refreshed correctly on theme change
    
    Change-Id: I29cdb02cf2be78257656c0a8e3e7b3005b40b205

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -405,6 +405,8 @@ public class NotificationPanelView extends PanelView implements
     }
 
     public void onThemeChanged() {
+        mQsNavbarScrim.setBackground(getResources().getDrawable(R.drawable.qs_navbar_scrim));
+
         // Re-inflate the status view group.
         int index = indexOfChild(mKeyguardStatusView);
         removeView(mKeyguardStatusView);
