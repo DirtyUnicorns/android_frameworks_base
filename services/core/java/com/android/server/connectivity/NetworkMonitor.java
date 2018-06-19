6183e8cbc7e3ddb261012824852bee0d4d465f7b
wloot <wlootlxt123@gmail.com>
2018-06-19 03:12:27 -0500

    Add special captive portal servers for chinese
    As we all know,google.com has been banned in China. So, we who are in China can't
    connect to the CaptivePortal server. This causesa  problem where All networks to include
    wifi and mobile data, are mistakenly detected as having no network connection.
    
    This solution sets the appropriate server by detecting the system default language
    
    *DU Edits*
    - Use our Util class to avoid having to add in extra method
    
    Change-Id: Ie5c9069050bbdc2aaf8587dbd61e7ae9aee04dc4

--- services/core/java/com/android/server/connectivity/NetworkMonitor.java
+++ services/core/java/com/android/server/connectivity/NetworkMonitor.java
@@ -64,6 +64,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.Protocol;
 import com.android.internal.util.State;
 import com.android.internal.util.StateMachine;
+import com.android.internal.util.du.Utils;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -100,6 +101,10 @@ public class NetworkMonitor extends StateMachine {
     private static final String DEFAULT_USER_AGENT    = "Mozilla/5.0 (X11; Linux x86_64) "
                                                       + "AppleWebKit/537.36 (KHTML, like Gecko) "
                                                       + "Chrome/60.0.3112.32 Safari/537.36";
+
+    private static final String DEFAULT_HTTPS_URL_CN     = "https://captive.v2ex.co/generate_204";
+    private static final String DEFAULT_HTTP_URL_CN      = "http://captive.v2ex.co/generate_204";
+    private static final String DEFAULT_FALLBACK_URL_CN  = "http://g.cn/generate_204";
 
     private static final int SOCKET_TIMEOUT_MS = 10000;
     private static final int PROBE_TIMEOUT_MS  = 3000;
@@ -706,17 +711,19 @@ public class NetworkMonitor extends StateMachine {
     }
 
     private static String getCaptivePortalServerHttpsUrl(Context context) {
-        return getSetting(context, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL, DEFAULT_HTTPS_URL);
+        return getSetting(context, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL,
+                         Utils.isChineseLanguage() ? DEFAULT_HTTPS_URL_CN : DEFAULT_HTTPS_URL);
     }
 
     public static String getCaptivePortalServerHttpUrl(Context context) {
-        return getSetting(context, Settings.Global.CAPTIVE_PORTAL_HTTP_URL, DEFAULT_HTTP_URL);
+        return getSetting(context, Settings.Global.CAPTIVE_PORTAL_HTTP_URL,
+                         Utils.isChineseLanguage() ? DEFAULT_HTTP_URL_CN : DEFAULT_HTTP_URL);
     }
 
     private URL[] makeCaptivePortalFallbackUrls(Context context) {
         String separator = ",";
         String firstUrl = getSetting(context,
-                Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL, DEFAULT_FALLBACK_URL);
+                Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL, Utils.isChineseLanguage() ? DEFAULT_FALLBACK_URL_CN : DEFAULT_FALLBACK_URL);
         String joinedUrls = firstUrl + separator + getSetting(context,
                 Settings.Global.CAPTIVE_PORTAL_OTHER_FALLBACK_URLS, DEFAULT_OTHER_FALLBACK_URLS);
         List<URL> urls = new ArrayList<>();
