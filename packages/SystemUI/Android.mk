e6c3633a451c1fc67ad8358a281c7972cabdd226
Alex <alex@dirtyunicorns.com>
2018-06-27 07:48:27 -0500

    Consolidated the DUI repos [1/2]
    Change-Id: I45710561588c3b7df78defbe04f3555e3ca42276

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -30,7 +30,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-Iaidl-files-under, src)
 
 
-LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../packages/apps/DUI/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/DUI/src/com/android/systemui/navigation)
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../packages/apps/SlimRecents/src)
 
@@ -62,7 +62,7 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-keyguard $(LOCAL_PATH)/res packages/apps/SlimRecents/res
-LOCAL_RESOURCE_DIR += packages/apps/DUI/res
+LOCAL_RESOURCE_DIR += external/DUI/res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
