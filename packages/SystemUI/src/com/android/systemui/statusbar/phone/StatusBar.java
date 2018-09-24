8f47017cf8e9637fcfb4d291d19ad37238a5937c
ezio84 <brabus84@gmail.com>
2018-09-24 18:53:30 -0500

    Cleanup: move theme/accent manager stuff out of StatusBar class
    kanging neobuddy89 and SpiritCroc idea
    
    https://github.com/crdroidandroid/android_frameworks_base/commit/f9db9b5c1a48fceaf5c212d2970c9282ca625466
    
    https://github.com/AICP/frameworks_base/commit/cc8deab922ec383ab4d551013645ddf28f0ce181
    
    Change-Id: I02d4934dceeb0d62b4bda9425acfa6d7e9504e3f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -218,6 +218,7 @@ import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.notification.AboveShelfObserver;
 import com.android.systemui.statusbar.notification.ActivityLaunchAnimator;
 import com.android.systemui.statusbar.notification.VisualStabilityManager;
+import com.android.systemui.statusbar.phone.ThemeAccentUtils;
 import com.android.systemui.statusbar.phone.UnlockMethodCache.OnUnlockMethodChangedListener;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.BatteryController.BatteryStateChangeCallback;
@@ -2073,28 +2074,11 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     public boolean isUsingDarkTheme() {
-        OverlayInfo themeInfo = null;
-        try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
-                    mLockscreenUserManager.getCurrentUserId());
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return themeInfo != null && themeInfo.isEnabled();
+        return ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     public void unloadStockDarkTheme() {
-        OverlayInfo themeInfo = null;
-        try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.systemui.theme.dark",
-                    mLockscreenUserManager.getCurrentUserId());
-            if (themeInfo != null && themeInfo.isEnabled()) {
-                mOverlayManager.setEnabled("com.android.systemui.theme.dark",
-                        false /*disable*/, mLockscreenUserManager.getCurrentUserId());
-            }
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        ThemeAccentUtils.unloadStockDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     @Nullable
@@ -3892,21 +3876,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
         if (isUsingDarkTheme() != useDarkTheme) {
             mUiOffloadThread.submit(() -> {
-                try {
-                    mOverlayManager.setEnabled("com.android.system.theme.dark",
-                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
-                    mOverlayManager.setEnabled("com.android.system.theme.dark",
-                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
-                    mOverlayManager.setEnabled("com.android.settings.theme.dark",
-                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
-                    mOverlayManager.setEnabled("com.android.settings.intelligence.theme.dark",
-                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
-                    if (useDarkTheme) {
-                        unloadStockDarkTheme();
-                    }
-                } catch (RemoteException e) {
-                    Log.w(TAG, "Can't change theme", e);
-                }
+                ThemeAccentUtils.setLightDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), useDarkTheme);
             });
         }
 
---
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ThemeAccentUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.phone;
+
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+
+public class ThemeAccentUtils {
+
+    public static final String TAG = "ThemeAccentUtils";
+
+    private static final String[] DARK_THEMES = {
+        "com.android.system.theme.dark", // 0
+        "com.android.settings.theme.dark", // 1
+        "com.android.settings.intelligence.theme.dark", // 2
+    };
+
+    private static final String STOCK_DARK_THEME = "com.android.systemui.theme.dark";
+
+    // Check for the dark system theme
+    public static boolean isUsingDarkTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(DARK_THEMES[0],
+                    userId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
+
+    // Unloads the stock dark theme
+    public static void unloadStockDarkTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(STOCK_DARK_THEME,
+                    userId);
+            if (themeInfo != null && themeInfo.isEnabled()) {
+                om.setEnabled(STOCK_DARK_THEME,
+                        false /*disable*/, userId);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
