12a90dddf3b5244f065a7eb32d99a62146d0f0aa
ezio84 <brabus84@gmail.com>
2018-11-21 22:31:39 +0100

    Battery icon: force show pct in QS header and when charging/powersave
    like aosp already wanted to do (QuickStatusBarHeader setForceShowPercent
    was being overridden by the tuner observer added by custom battery
    commits to BatteryMeterView).
    
    This is also good for users that don't want the percentage in the
    statusbar but still want to be able to check it in the QS panel.
    
    
    PS 3: Fix percentage not showing in qs/keyguard after screen off / on
    and force it only when hidden icon style or hidden percentage option
    are enabled
    
    Change-Id: I57cdd6c158af8440ed3372c7799875d34fd58f75

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -72,6 +72,8 @@ public class BatteryMeterView extends LinearLayout implements
     private int mTextColor;
     private int mLevel;
     private boolean mForceShowPercent;
+    private boolean misQsbHeader;
+    private int mShowPercent;
     private boolean mShowPercentAvailable;
     private boolean mCharging;
     private boolean mPowerSave;
@@ -240,7 +242,9 @@ public class BatteryMeterView extends LinearLayout implements
         // text battery will always show percentage
         if (isCircleBattery()
                 || getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT) {
-            setForceShowPercent(pluggedIn);
+            if (!alwaysShowPercentage()) {
+                setForceShowPercent(pluggedIn);
+            }
             // mDrawable.setCharging(pluggedIn) will invalidate the view
         }
 
@@ -284,15 +288,20 @@ public class BatteryMeterView extends LinearLayout implements
         }
     }
 
+    private boolean alwaysShowPercentage() {
+        return misQsbHeader
+                && (getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN
+                || (getMeterStyle() != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN && mShowPercent == 0/*hidden*/));
+    }
+
     private void updateShowPercent() {
         final boolean showing = mBatteryPercentView != null;
         int style = Settings.System.getIntForUser(getContext().getContentResolver(),
                 SHOW_BATTERY_PERCENT, 0, mUser);
-        if (mForceShowPercent
-                || (getMeterStyle() != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN
-                && (mForceShowPercent || mPowerSave || mCharging))) {
-            style = 1; // Default view
-        }
+        mShowPercent = style;
+
+        boolean showAnyway = alwaysShowPercentage() || mPowerSave || mCharging;
+        if (showAnyway) style = 1; // Default view
         switch (style) {
             case 1:
                 if (!showing) {
@@ -414,12 +423,16 @@ public class BatteryMeterView extends LinearLayout implements
         }
     }
 
+    public void isQsbHeader() {
+        misQsbHeader = true;
+    }
+
     private void updateBatteryStyle(String styleStr) {
         final int style = styleStr == null ?
                 BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT : Integer.parseInt(styleStr);
         mDrawable.setMeterStyle(style);
 
-        mForceShowPercent = false;
+        mForceShowPercent = alwaysShowPercentage() ? true : false;
 
         switch (style) {
             case BatteryMeterDrawableBase.BATTERY_STYLE_TEXT:
