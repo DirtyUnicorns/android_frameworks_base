2571a99f422d37b8e22d6123c94af009c549753b
Alex Cruz <alex@dirtyunicorns.com>
2018-11-27 22:12:39 -0600

    Change default wallpaper component name
    Reason for this change is the same for why we changed the gesture
    settings config, to avoid issues with the vendor partition on devices
    like the Pixel 3/3XL. Instead of recompiling a vendor and having users
    flash this 'special' one every time they use DU, we figured this is
    best course of action.
    
    The problem that led to this was that Google was overriding the default
    wallpaper component string to show some shitty live wallpaper. We don't
    want that. We our default wallpaper to show.
    
    Change-Id: Ic3ba6e0485ae91c041633768babefe19d49ea142

--- core/java/android/app/WallpaperManager.java
+++ core/java/android/app/WallpaperManager.java
@@ -1882,7 +1882,7 @@ public class WallpaperManager {
             }
         }
 
-        flat = context.getString(com.android.internal.R.string.default_wallpaper_component);
+        flat = context.getString(com.android.internal.R.string.default_wallpaper_component_custom);
         if (!TextUtils.isEmpty(flat)) {
             final ComponentName cn = ComponentName.unflattenFromString(flat);
             if (cn != null) {
