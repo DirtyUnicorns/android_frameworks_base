57b7b5f9dc66a43cc3930ceadbd82fae7c4b505b
ezio84 <brabus84@gmail.com>
2018-12-27 21:42:51 +0100

    Weather client: fix refresh at boot and improve/cleanup code [1/2]
    2/2 is in the buildable WeatherClient app source
    
    code cleanup
    
    remove slow package manager isAvailable checks
    (e.g. addWeather is called from onBindSlice that must be a
    very fast operation as per google api guidelines for the slice provider,
    so it's better to not call packagemanager there)
    we anyway have a null check for the Cursor query so we are npe free
    if the user removes the package
    
    remove query call in the weatherclient addobserver because when
    keyguardsliceprovider is created (before the user unlocks the device)
    the weather provider is not available yet.
    The on boot receiver intent does the job instead, and it's triggered
    after the user unlocks the device
    
    cleanup resetScheduledAlarm calls code
    (ACTION_TIME_CHANGED was calling it twice, once straight then
    updateWeatherAndNotify that calls it again)
    
    cleanup addObserver code
    
    Change-Id: Icd8b0eff4fe88cc15a32196c043a372c17d6095f

--- core/java/com/android/internal/util/custom/weather/WeatherClient.java
+++ core/java/com/android/internal/util/custom/weather/WeatherClient.java
@@ -43,8 +43,7 @@ public class WeatherClient {
 
     public static final String SERVICE_PACKAGE = "org.pixelexperience.weather.client";
     private static final String SERVICE_PACKAGE_PERMISSION = SERVICE_PACKAGE + ".READ_WEATHER";
-    public static final Uri WEATHER_URI =
-            Uri.parse("content://org.pixelexperience.weather.client.provider/weather");
+    public static final Uri WEATHER_URI = Uri.parse("content://org.pixelexperience.weather.client.provider/weather");
     public static final int WEATHER_UPDATE_SUCCESS = 0; // Success
     public static final int WEATHER_UPDATE_RUNNING = 1; // Update running
     public static final int WEATHER_UPDATE_ERROR = 2; // Error
@@ -60,6 +59,8 @@ public class WeatherClient {
             COLUMN_TEMPERATURE_METRIC,
             COLUMN_TEMPERATURE_IMPERIAL
     };
+
+    private boolean mBootAndUnlockDone;
 
     private static final int WEATHER_UPDATE_INTERVAL = 60 * 20 * 1000; // 20 minutes
     private String updateIntentAction;
@@ -83,13 +84,14 @@ public class WeatherClient {
                 onScreenOff();
             } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
                 onScreenOn();
-            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
-                    updateIntentAction.equals(intent.getAction())) {
-                updateWeatherAndNotify();
-            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction()) ||
-                    Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
-                resetScheduledAlarm();
-                updateWeatherAndNotify();
+            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+                mBootAndUnlockDone = true;
+                updateWeatherAndNotify(false);
+            } else if (updateIntentAction.equals(intent.getAction())) {
+                updateWeatherAndNotify(false);
+            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction())
+                    || Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
+                updateWeatherAndNotify(true);
             }
         }
     };
@@ -99,8 +101,7 @@ public class WeatherClient {
         mContext.enforceCallingOrSelfPermission(SERVICE_PACKAGE_PERMISSION,
                 "Missing or invalid weather permission: " + SERVICE_PACKAGE_PERMISSION);
         updateIntentAction = "updateIntentAction_" + Integer.toString(getRandomInt());
-        pendingWeatherUpdate = PendingIntent.getBroadcast(
-                mContext, getRandomInt(), new Intent(updateIntentAction), 0);
+        pendingWeatherUpdate = PendingIntent.getBroadcast(mContext, getRandomInt(), new Intent(updateIntentAction), 0);
         mObserver = new ArrayList<>();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -113,25 +114,16 @@ public class WeatherClient {
         mContext.registerReceiver(weatherReceiver, filter);
     }
 
-    public static boolean isAvailable(Context context) {
-        final PackageManager pm = context.getPackageManager();
-        try {
-            pm.getPackageInfo(SERVICE_PACKAGE, PackageManager.GET_ACTIVITIES);
-            int enabled = pm.getApplicationEnabledSetting(SERVICE_PACKAGE);
-            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
-                    enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
-    }
-
     private int getRandomInt() {
         Random r = new Random();
         return r.nextInt((20000000 - 10000000) + 1) + 10000000;
     }
 
-    private void updateWeatherAndNotify() {
-        if (isRunning){
+    private void updateWeatherAndNotify(boolean forceResetSchedule) {
+        if (!mBootAndUnlockDone) return;
+
+        if (isRunning) {
+            if (forceResetSchedule) resetScheduledAlarm();
             return;
         }
         isRunning = true;
@@ -154,13 +146,12 @@ public class WeatherClient {
     }
 
     private boolean needsUpdate() {
-        boolean lastUpdatedExpired =
-                System.currentTimeMillis() - lastUpdated > WEATHER_UPDATE_INTERVAL;
+        boolean lastUpdatedExpired = System.currentTimeMillis() - lastUpdated > WEATHER_UPDATE_INTERVAL;
         return mWeatherInfo.getStatus() != WEATHER_UPDATE_SUCCESS || lastUpdatedExpired;
     }
 
     private void onScreenOn() {
-        if (isScreenOn){
+        if (!mBootAndUnlockDone || isScreenOn){
             return;
         }
         if (DEBUG) Log.d(TAG, "onScreenOn");
@@ -168,7 +159,7 @@ public class WeatherClient {
         if (!isRunning) {
             if (needsUpdate()) {
                 if (DEBUG) Log.d(TAG, "Needs update, triggering updateWeatherAndNotify");
-                updateWeatherAndNotify();
+                updateWeatherAndNotify(false);
             } else {
                 if (DEBUG) Log.d(TAG, "Scheduling update");
                 scheduleWeatherUpdateAlarm();
@@ -230,28 +221,13 @@ public class WeatherClient {
         } else {
             mWeatherInfo.status = WEATHER_UPDATE_ERROR;
         }
+
         if (DEBUG) Log.d(TAG, mWeatherInfo.toString());
         isRunning = false;
     }
 
     public void addObserver(final WeatherObserver observer) {
         mObserver.add(observer);
-        if (isRunning) {
-            return;
-        }
-        isRunning = true;
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                updateWeatherData();
-                try {
-                    observer.onWeatherUpdated(mWeatherInfo);
-                } catch (Exception ignored) {
-                }
-            }
-        });
-        thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
     }
 
     public void removeObserver(WeatherObserver observer) {
@@ -305,14 +281,10 @@ public class WeatherClient {
             conditions.put("rain", R.drawable.weather_rain);
             conditions.put("windy", R.drawable.weather_windy);
             conditions.put("snow", R.drawable.weather_snow);
-            conditions.put("scattered-thunderstorms",
-                    R.drawable.weather_isolated_scattered_thunderstorms);
-            conditions.put("scattered-thunderstorms-night",
-                    R.drawable.weather_isolated_scattered_thunderstorms_night);
-            conditions.put("isolated-thunderstorms",
-                    R.drawable.weather_isolated_scattered_thunderstorms);
-            conditions.put("isolated-thunderstorms-night",
-                    R.drawable.weather_isolated_scattered_thunderstorms_night);
+            conditions.put("scattered-thunderstorms", R.drawable.weather_isolated_scattered_thunderstorms);
+            conditions.put("scattered-thunderstorms-night", R.drawable.weather_isolated_scattered_thunderstorms_night);
+            conditions.put("isolated-thunderstorms", R.drawable.weather_isolated_scattered_thunderstorms);
+            conditions.put("isolated-thunderstorms-night", R.drawable.weather_isolated_scattered_thunderstorms_night);
             conditions.put("thunderstorms", R.drawable.weather_thunderstorms);
             conditions.put("foggy", R.drawable.weather_foggy);
             for (String condition : conditions.keySet()) {
