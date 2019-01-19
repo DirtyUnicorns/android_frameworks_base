3466cfe339684c2135b3c4b57b7c6a6ad84766fe
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 01:26:32 -0600

    Battery icon: Restore stock behavior in the keyguard
    This allows the keyguard to once more follow along with the regular
    statusbar setting. Previously this was forced on no matter what.
    
    Change-Id: Ib10218f2eced0d5dd9e03e3b5312c717e320fd1b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -108,7 +108,6 @@ public class KeyguardStatusBarView extends RelativeLayout
         mMultiUserAvatar = findViewById(R.id.multi_user_avatar);
         mCarrierLabel = findViewById(R.id.keyguard_carrier_text);
         mBatteryView = mSystemIconsContainer.findViewById(R.id.battery);
-        mBatteryView.isQsbHeader();
         mCutoutSpace = findViewById(R.id.cutout_space_view);
         mStatusIconArea = findViewById(R.id.status_icon_area);
 
@@ -190,6 +189,7 @@ public class KeyguardStatusBarView extends RelativeLayout
                 mMultiUserSwitch.setVisibility(View.GONE);
             }
         }
+        mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
     }
 
     private void updateSystemIconsLayoutParams() {
