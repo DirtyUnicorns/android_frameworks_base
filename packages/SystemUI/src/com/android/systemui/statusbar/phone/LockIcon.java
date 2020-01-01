DirtyUnicorns/android_frameworks_base

commit bb54f0a3efd99d37eb74a8c604dfb47544c81242

Keyguard: Lock icon improvements
There was a ~300ms delay (I think) between the actual dismissal of the lock icon from keyguard locked to unlocked
(only with no security method activated). So, mKeyguardIndicationController.setVisible(false) has been moved to
KeyguardgoingAway() to anticipate the icon dismissal with a small setAlpha hack in KeyguardIndicationController.

Pulse: Don't show the icon here, it's stupid.

Change-Id: Ia926cd27e96bb450ed8f5ed9c686556fd647fa45

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -266,7 +266,7 @@ public class StatusBarWindowView extends FrameLayout {
      */
     public void setPulsing(boolean pulsing) {
         if (mLockIcon != null) {
-            mLockIcon.setPulsing(pulsing);
+            mLockIcon.setVisibility(pulsing ? View.GONE : View.VISIBLE);
         }
     }
 

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -259,9 +259,11 @@ public class KeyguardIndicationController implements StateListener,
                 hideTransientIndication();
             }
             updateIndication(false);
+            mLockIcon.setAlpha(255);
         } else if (!visible) {
             // If we unlock and return to keyguard quickly, previous error should not be shown
             hideTransientIndication();
+            mLockIcon.setAlpha(0);
         }
     }
 

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3354,6 +3354,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void keyguardGoingAway() {
         // Treat Keyguard exit animation as an app transition to achieve nice transition for status
         // bar.
+        mKeyguardIndicationController.setVisible(false);
         mKeyguardMonitor.notifyKeyguardGoingAway(true);
         mCommandQueue.appTransitionPending(mDisplayId, true /* forced */);
     }
@@ -3572,7 +3573,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mAmbientIndicationContainer.setVisibility(View.VISIBLE);
             }
         } else {
-            mKeyguardIndicationController.setVisible(false);
             if (mKeyguardUserSwitcher != null) {
                 mKeyguardUserSwitcher.setKeyguard(false,
                         mStatusBarStateController.goingToFullShade() ||

commit 827284b2e0c44244c1fa173c85cdf9bbbb237d10

Fix lock icon being stuck in AoD after pulsing notifications
* This got broken after bb54f0a3
* We needed to account for transitions from AoD to notification pulse back to AoD
again

Change-Id: Ib250548596ae5fdc4a7fdb863646909608969363

packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
@@ -533,8 +533,9 @@ public class LockIcon extends KeyguardAffordanceView implements OnUserInfoChange
      * @param pulsing {@code true} when pulsing.
      */
     public void setPulsing(boolean pulsing) {
-        mPulsing = pulsing;
-        update();
+        /*mPulsing = pulsing;
+        update();*/
+        // do nothing, just behave like when aod dozing
     }
 
     /**

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -266,7 +266,7 @@ public class StatusBarWindowView extends FrameLayout {
      */
     public void setPulsing(boolean pulsing) {
         if (mLockIcon != null) {
-            mLockIcon.setVisibility(pulsing ? View.GONE : View.VISIBLE);
+            mLockIcon.setPulsing(pulsing);
         }
     }
 