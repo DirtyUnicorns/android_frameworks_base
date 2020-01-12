799ace14dab0820a67b665ef7cf5e6f3d36f21c1
Alex <du.alexcruz@gmail.com>
2020-01-12 01:41:38 +0000

    AmbientDisplayConfiguration: Fix public API compatibility issue
    /home/cruz/duq/frameworks/base/core/java/android/hardware/display/AmbientDisplayConfiguration.java:230: error: Added method android.hardware.display.AmbientDisplayConfiguration.deviceHasSoli() to the test API [AddedMethod]
    /home/cruz/duq/frameworks/base/core/java/android/hardware/display/AmbientDisplayConfiguration.java:220: error: Added method android.hardware.display.AmbientDisplayConfiguration.isAmbientGestureEnabled(int) to the test API [AddedMethod]
    /home/cruz/duq/frameworks/base/core/java/android/hardware/display/AmbientDisplayConfiguration.java:225: error: Added method android.hardware.display.AmbientDisplayConfiguration.isAmbientTickerEnabled(int) to the test API [AddedMethod]
    
    Change-Id: I2492aefcccaa861612699f6c8e2282f01ffb521d

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -217,16 +217,19 @@ public class AmbientDisplayConfiguration {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(), name, def, user) != 0;
     }
 
+    /** {@hide} */
     public boolean isAmbientGestureEnabled(int user) {
         return !mDeviceHasSoli && Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.AMBIENT_WAKE_GESTURES, 1, user) != 0;
     }
 
+    /** {@hide} */
     public boolean isAmbientTickerEnabled(int user) {
         return Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 
+    /** {@hide} */
     public boolean deviceHasSoli() {
         return mDeviceHasSoli;
     }
