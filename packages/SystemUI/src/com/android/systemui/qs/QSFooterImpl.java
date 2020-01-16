03f4c11fe2e485f5a3af6846983e8bdd778cdfc1
rajadeja <raj.jdja@gmail.com>
2020-01-16 18:13:24 +0000

    QSFooterImpl: Animate multiuser avatar
    Change-Id: I825b5f2271ac7051c5260a14ba2b55dc91b43c56

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -225,6 +225,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private TouchAnimator createFooterAnimator() {
         return new TouchAnimator.Builder()
                 .addFloat(mActionsContainer, "alpha", 0, 1)
+                .addFloat(mMultiUserAvatar, "alpha", 0, 1)
                 .addFloat(mEditContainer, "alpha", 0, 1)
                 .addFloat(mDragHandle, "alpha", 1, 0, 0)
                 .addFloat(mPageIndicator, "alpha", 0, 1)
