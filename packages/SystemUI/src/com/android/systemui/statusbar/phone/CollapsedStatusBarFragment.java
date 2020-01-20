2e8ff8fb13d07e4f2d14e20f6d2fa92f6941c18b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2020-01-19 19:56:23 -0500

    CarrierLabel: Keyguard/Statusbar carrier label options
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    
    PureNexus Edits:
    * adapted to replace the carrier label toggle in PureNexus-OLD/android_frameworks_base@16a247a
    * this now provides more robust options for carrier label customization
    
    AICP brought up to O by AliB:
    - Adapted to O
    
    Change-Id: I6a73a15160c8334f90feb3a26906a13071097ed7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -98,7 +98,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                     Settings.System.STATUS_BAR_LOGO),
                     false, this, UserHandle.USER_ALL);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_CARRIER),
+                    Settings.System.STATUS_BAR_SHOW_CARRIER),
                     false, this, UserHandle.USER_ALL);
         }
 
@@ -236,8 +236,10 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if ((diff1 & DISABLE_NOTIFICATION_ICONS) != 0) {
             if ((state1 & DISABLE_NOTIFICATION_ICONS) != 0) {
                 hideNotificationIconArea(animate);
+                hideCarrierName(animate);
             } else {
                 showNotificationIconArea(animate);
+                showCarrierName(animate);
             }
         }
         // The clock may have already been hidden, but we might want to shift its
@@ -446,12 +448,32 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         disable(getContext().getDisplayId(), mDisabled1, mDisabled1, false /* animate */);
     }
 
+    public void hideCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            animateHide(mCustomCarrierLabel, animate, true);
+        }
+    }
+
+    public void showCarrierName(boolean animate) {
+        if (mCustomCarrierLabel != null) {
+            setCarrierLabel(animate);
+        }
+    }
+
+    private void setCarrierLabel(boolean animate) {
+        if (mShowCarrierLabel == 2 || mShowCarrierLabel == 3) {
+            animateShow(mCustomCarrierLabel, animate);
+        } else {
+            animateHide(mCustomCarrierLabel, animate, false);
+        }
+    }
+
     public void updateSettings(boolean animate) {
         mShowLogo = Settings.System.getIntForUser(
                 mContentResolver, Settings.System.STATUS_BAR_LOGO, 0,
                 UserHandle.USER_CURRENT) == 1;
         mShowCarrierLabel = Settings.System.getIntForUser(
-                mContentResolver, Settings.System.STATUS_BAR_CARRIER, 1,
+                mContentResolver, Settings.System.STATUS_BAR_SHOW_CARRIER, 1,
                 UserHandle.USER_CURRENT);
         if (mNotificationIconAreaInner != null) {
             if (mShowLogo) {
@@ -462,5 +484,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 animateHiddenState(mDULogo, View.GONE, animate);
             }
         }
+        setCarrierLabel(animate);
     }
 }
