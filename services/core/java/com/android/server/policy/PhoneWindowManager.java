c839c41f288c53a17c4dfcfb55ed5be92bc551da
Alex <du.alexcruz@gmail.com>
2020-02-21 22:27:21 -0500

    Navbar: Remove in-app search action
    The in-app seach action is outdated and not all useful. Fight me!
    
    Change-Id: I5a1cff8707540978b6392f1fee8fe034f14dd4ad

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6448,9 +6448,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 triggerVirtualKeypress(KeyEvent.KEYCODE_MENU, false);
                 break;
             case NavbarUtilities.KEY_ACTION_BACK:
-            case NavbarUtilities.KEY_ACTION_IN_APP_SEARCH:
-                triggerVirtualKeypress(keyCode, false);
-                break;
             case NavbarUtilities.KEY_ACTION_APP_SWITCH:
                 toggleRecentApps();
                 break;
