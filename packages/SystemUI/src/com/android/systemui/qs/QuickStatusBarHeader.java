eda90669120f19371f3b6310a64e641762266633
Nicolas Dhouailly <ndhouailly%2540gmail.com>
2020-04-27 18:55:56 +0200

    Battery Styles: Remove unneeded function parameter in BatteryMeterView
    - Previously, we added a boolean parameter in the updateShowPercent
    function that was used for the battery percentage in the quick status bar header.
    With the new battery style code, this boolean is no longer needed.
    Let's remove all the code that was affected by this change.
    
    Change-Id: I29a1b894253a67812dcd8e4c0399a0ebe837ff8a

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -348,7 +348,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     }
 
     public void setBatteryPercentMode() {
-        mBatteryRemainingIcon.setPercentShowMode(getBatteryPercentMode(), true);
+        mBatteryRemainingIcon.setPercentShowMode(getBatteryPercentMode());
     }
 
     public void setExpanded(boolean expanded) {
