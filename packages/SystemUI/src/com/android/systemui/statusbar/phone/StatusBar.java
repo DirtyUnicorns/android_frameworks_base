d6daeab280cbf4c23d8b5d789bf9b0afc55f7dc3
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-06-15 21:54:49 +0000

    Pulse: Add automatic album art color mode [1/3]
    Change-Id: I513b7a96c3eea9c79b953ace1430553495c57970

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -740,9 +740,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         mCommandQueue = getComponent(CommandQueue.class);
         mCommandQueue.addCallback(this);
 
-        // this will initialize Pulse and begin listening for media events
-        mMediaManager.addCallback(Dependency.get(PulseController.class));
-
         RegisterStatusBarResult result = null;
         try {
             result = mBarService.registerStatusBar(mCommandQueue);
@@ -845,6 +842,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         mDefaultHome = getCurrentDefaultHome();
         mContext.registerReceiver(mDefaultHomeBroadcastReceiver, mDefaultHomeIntentFilter);
         ActivityManagerWrapper.getInstance().registerTaskStackListener(mTaskStackChangeListener);
+
+        // this will initialize Pulse and begin listening for media events
+        mMediaManager.addCallback(Dependency.get(PulseController.class));
     }
 
     // ================================================================================
