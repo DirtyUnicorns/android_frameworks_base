d2816306aab21abb96e5acd1375bbc73d42ff340
ezio84 <brabus84@gmail.com>
2020-10-14 22:10:25 +0200

    Use du.fingerprint instead of build.date to clear cache
    Change-Id: Ic69c720f8a6f0821170f58fd1163889e6752f68f

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3902,7 +3902,7 @@ public class PackageManagerService extends IPackageManager.Stub
         // feature flags should cause us to invalidate any caches.
         final String cacheName = FORCE_PACKAGE_PARSED_CACHE_ENABLED ? "debug"
                 : SystemProperties.digestOf(
-                        "ro.build.date",
+                        "ro.du.fingerprint",
                         StorageManager.PROP_ISOLATED_STORAGE,
                         StorageManager.PROP_ISOLATED_STORAGE_SNAPSHOT
                 );
